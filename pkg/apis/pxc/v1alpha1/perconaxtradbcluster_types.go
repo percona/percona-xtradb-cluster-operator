4bfccdfd9ec80d54962140b8062759d530be6b7a
Andrew Pogrebnoy <absourd.noise@gmail.com>
2018-12-12 22:31:20 +0200

    Generated stub api and controller

---
+++ pkg/apis/pxc/v1alpha1/perconaxtradbcluster_types.go
@@ -0,0 +1,45 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// PerconaXtraDBClusterSpec defines the desired state of PerconaXtraDBCluster
+type PerconaXtraDBClusterSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+}
+
+// PerconaXtraDBClusterStatus defines the observed state of PerconaXtraDBCluster
+type PerconaXtraDBClusterStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PerconaXtraDBCluster is the Schema for the perconaxtradbclusters API
+// +k8s:openapi-gen=true
+type PerconaXtraDBCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PerconaXtraDBClusterSpec   `json:"spec,omitempty"`
+	Status PerconaXtraDBClusterStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PerconaXtraDBClusterList contains a list of PerconaXtraDBCluster
+type PerconaXtraDBClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []PerconaXtraDBCluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&PerconaXtraDBCluster{}, &PerconaXtraDBClusterList{})
+}

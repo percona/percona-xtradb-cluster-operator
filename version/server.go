1a5432a0cd901c30534c700fdc23ae52c0aa94b0
Andrew Pogrebnoy <absourd.noise@gmail.com>
2019-01-02 22:06:25 +0200

    Use internal clientcmd package to get REST-client for server version defining

--- version/server.go
+++ version/server.go
@@ -2,15 +2,13 @@ package version
 
 import (
 	"encoding/json"
-	"net"
-	"os"
+	"fmt"
 	"sync"
 
 	k8sversion "k8s.io/apimachinery/pkg/version"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 
+	"github.com/Percona-Lab/percona-xtradb-cluster-operator/clientcmd"
 	api "github.com/Percona-Lab/percona-xtradb-cluster-operator/pkg/apis/pxc/v1alpha1"
 )
 
@@ -40,11 +38,13 @@ func Server() (*api.ServerVersion, error) {
 
 // GetServer make request to platform server and returns server version and platform (k8s|oc)
 func GetServer() (*api.ServerVersion, error) {
-	kubeClient, _ := mustNewKubeClientAndConfig()
-	client := kubeClient.Discovery().RESTClient()
+	cl, err := clientcmd.NewClient()
+	if err != nil {
+		return nil, fmt.Errorf("create REST client: %v", err)
+	}
+	client := cl.REST()
 
 	version := &api.ServerVersion{}
-	var err error
 	// oc 3.9
 	version.Info, err = probeAPI("/version/openshift", client)
 	if err == nil {
@@ -83,43 +83,4 @@ func probeAPI(path string, client rest.Interface) (k8sversion.Info, error) {
 	}
 
 	return vInfo, nil
-}
-
-// mustNewKubeClientAndConfig returns the in-cluster config and kubernetes client
-// or if KUBERNETES_CONFIG is given an out of cluster config and client
-func mustNewKubeClientAndConfig() (kubernetes.Interface, *rest.Config) {
-	var cfg *rest.Config
-	var err error
-	if os.Getenv("KUBERNETES_CONFIG") != "" {
-		cfg, err = outOfClusterConfig()
-	} else {
-		cfg, err = inClusterConfig()
-	}
-	if err != nil {
-		panic(err)
-	}
-	return kubernetes.NewForConfigOrDie(cfg), cfg
-}
-
-// inClusterConfig returns the in-cluster config accessible inside a pod
-func inClusterConfig() (*rest.Config, error) {
-	// Work around https://github.com/kubernetes/kubernetes/issues/40973
-	// See https://github.com/coreos/etcd-operator/issues/731#issuecomment-283804819
-	if len(os.Getenv("KUBERNETES_SERVICE_HOST")) == 0 {
-		addrs, err := net.LookupHost("kubernetes.default.svc")
-		if err != nil {
-			return nil, err
-		}
-		os.Setenv("KUBERNETES_SERVICE_HOST", addrs[0])
-	}
-	if len(os.Getenv("KUBERNETES_SERVICE_PORT")) == 0 {
-		os.Setenv("KUBERNETES_SERVICE_PORT", "443")
-	}
-	return rest.InClusterConfig()
-}
-
-func outOfClusterConfig() (*rest.Config, error) {
-	kubeconfig := os.Getenv("KUBERNETES_CONFIG")
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	return config, err
 }

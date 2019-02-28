0d4fabc6bea84d23a6e5cd4e2242789a32db2d6e
Mykola Marzhan <mykola.marzhan@percona.com>
2019-02-28 08:47:02 +0200

    add poddisruptionbudgets policy

--- pkg/apis/pxc/v1alpha1/perconaxtradbcluster_types.go
+++ pkg/apis/pxc/v1alpha1/perconaxtradbcluster_types.go
@@ -7,7 +7,6 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	policyv1beta1 "k8s.io/api/policy/v1beta1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	k8sversion "k8s.io/apimachinery/pkg/version"

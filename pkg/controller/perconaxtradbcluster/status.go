c8e3aae5e27269ff98b20c4d8ed3e875da67e526
Andrew Pogrebnoy <absourd.noise@gmail.com>
2019-04-05 23:57:52 +0300

    Extend cluster status with [error] messages

--- pkg/controller/perconaxtradbcluster/status.go
+++ pkg/controller/perconaxtradbcluster/status.go
@@ -3,37 +3,50 @@ package perconaxtradbcluster
 import (
 	"context"
 	"fmt"
+	"time"
 
-	"github.com/percona/percona-xtradb-cluster-operator/pkg/pxc/app/statefulset"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	api "github.com/percona/percona-xtradb-cluster-operator/pkg/apis/pxc/v1alpha1"
+	"github.com/percona/percona-xtradb-cluster-operator/pkg/pxc/app/statefulset"
 )
 
 func (r *ReconcilePerconaXtraDBCluster) updateStatus(cr *api.PerconaXtraDBCluster) (err error) {
-	cr.Status.PXC, err = r.componentStatus(statefulset.NewNode(cr), cr.Spec.PXC, cr.Namespace)
+	cr.Status = api.PerconaXtraDBClusterStatus{}
+
+	cr.Status.PXC, err = r.appStatus(statefulset.NewNode(cr), cr.Spec.PXC, cr.Namespace)
 	if err != nil {
 		return fmt.Errorf("get pxc status: %v", err)
 	}
 
-	if cr.Status.PXC.Size == cr.Status.PXC.Ready {
-		cr.Status.Status = api.ClusterStateReady
+	cr.Status.Host = cr.Name + "-" + "pxc"
+	if cr.Status.PXC.Message != "" {
+		cr.Status.Messages = append(cr.Status.Messages, "PXC: "+cr.Status.PXC.Message)
 	}
 
-	cr.Status.Host = cr.Name + "-" + "pxc"
-
 	if cr.Spec.ProxySQL != nil && cr.Spec.ProxySQL.Enabled {
-		cr.Status.ProxySQL, err = r.componentStatus(statefulset.NewProxy(cr), cr.Spec.ProxySQL, cr.Namespace)
+		cr.Status.ProxySQL, err = r.appStatus(statefulset.NewProxy(cr), cr.Spec.ProxySQL, cr.Namespace)
 		if err != nil {
 			return fmt.Errorf("get proxysql status: %v", err)
 		}
-		if cr.Status.ProxySQL.Size != cr.Status.ProxySQL.Ready {
-			cr.Status.Status = api.ClusterStateInit
-		}
 
 		cr.Status.Host = cr.Name + "-" + "proxysql"
+		if cr.Status.ProxySQL.Message != "" {
+			cr.Status.Messages = append(cr.Status.Messages, "ProxySQL: "+cr.Status.ProxySQL.Message)
+		}
+	}
+
+	switch {
+	case cr.Status.PXC.Status == cr.Status.ProxySQL.Status:
+		cr.Status.Status = cr.Status.PXC.Status
+	case cr.Status.PXC.Status == api.AppStateError || cr.Status.ProxySQL.Status == api.AppStateError:
+		cr.Status.Status = api.AppStateError
+	case cr.Status.PXC.Status == api.AppStateInit || cr.Status.ProxySQL.Status == api.AppStateInit:
+		cr.Status.Status = api.AppStateInit
+	default:
+		cr.Status.Status = api.AppStateUnknown
 	}
 
 	err = r.client.Status().Update(context.TODO(), cr)
@@ -49,7 +62,7 @@ func (r *ReconcilePerconaXtraDBCluster) updateStatus(cr *api.PerconaXtraDBCluste
 	return nil
 }
 
-func (r *ReconcilePerconaXtraDBCluster) componentStatus(app api.App, podSpec *api.PodSpec, namespace string) (api.PodStatus, error) {
+func (r *ReconcilePerconaXtraDBCluster) appStatus(app api.App, podSpec *api.PodSpec, namespace string) (api.AppStatus, error) {
 	list := corev1.PodList{}
 	err := r.client.List(context.TODO(),
 		&client.ListOptions{
@@ -59,18 +72,39 @@ func (r *ReconcilePerconaXtraDBCluster) componentStatus(app api.App, podSpec *ap
 		&list,
 	)
 	if err != nil {
-		return api.PodStatus{}, fmt.Errorf("get list: %v", err)
+		return api.AppStatus{}, fmt.Errorf("get list: %v", err)
 	}
 
-	var status api.PodStatus
-	status.Size = podSpec.Size
+	status := api.AppStatus{
+		Size:   podSpec.Size,
+		Status: api.AppStateInit,
+	}
 
 	for _, pod := range list.Items {
-		for _, cs := range pod.Status.ContainerStatuses {
-			if cs.Name == app.Labels()["app.kubernetes.io/component"] && cs.Ready {
-				status.Ready++
+		for _, cond := range pod.Status.Conditions {
+			switch cond.Type {
+			case corev1.ContainersReady:
+				if cond.Status == corev1.ConditionTrue {
+					status.Ready++
+				} else if cond.Status == corev1.ConditionFalse {
+					for _, cntr := range pod.Status.ContainerStatuses {
+						if cntr.State.Waiting != nil && cntr.State.Waiting.Message != "" {
+							status.Message += cntr.Name + ": " + cntr.State.Waiting.Message + "; "
+						}
+					}
+				}
+			case corev1.PodScheduled:
+				if cond.Reason == corev1.PodReasonUnschedulable &&
+					cond.LastTransitionTime.Time.Before(time.Now().Add(-1*time.Minute)) {
+					status.Status = api.AppStateError
+					status.Message = cond.Message
+				}
 			}
 		}
+	}
+
+	if status.Size == status.Ready {
+		status.Status = api.AppStateReady
 	}
 
 	return status, nil

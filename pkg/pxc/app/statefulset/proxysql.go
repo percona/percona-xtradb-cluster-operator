141033f35246cc8a33afdca71aa16fd88bf0fa14
shanbiao.jsb <shanbiao.jsb@alibaba-inc.com>
2019-05-05 15:09:41 +0800

    rollback commits

--- pkg/pxc/app/statefulset/proxysql.go
+++ pkg/pxc/app/statefulset/proxysql.go
@@ -6,7 +6,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	api "github.com/percona/percona-xtradb-cluster-operator/pkg/apis/pxc/v1alpha1"
-	"github.com/percona/percona-xtradb-cluster-operator/pkg/pxc/app"
+	app "github.com/percona/percona-xtradb-cluster-operator/pkg/pxc/app"
 )
 
 const (

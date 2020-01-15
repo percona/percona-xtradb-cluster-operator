70dd761d36f9314640c8c372b83e83e92e529f3c
paval <pavel.kasko@percona.com>
2020-01-15 10:27:51 +0200

    CLOUD-429: debug

--- pkg/apis/pxc/v1/pxc_types.go
+++ pkg/apis/pxc/v1/pxc_types.go
@@ -329,11 +329,6 @@ func (cr *PerconaXtraDBCluster) CheckNSetDefaults(serverVersion *ServerVersion) 
 			c.PXC.SSLInternalSecretName = cr.Name + "-ssl-internal"
 		}
 
-		// number of pxc replicas should be an odd
-		if c.PXC.Size%2 == 0 && !c.PXC.AllowUnsafeConfig {
-			c.PXC.Size++
-		}
-
 		// Set maxUnavailable = 1 by default for PodDisruptionBudget-PXC.
 		// It's a description of the number of pods from that set that can be unavailable after the eviction.
 		if c.PXC.PodDisruptionBudget == nil {

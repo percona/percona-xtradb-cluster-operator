33f2421c2893b45abdd006e36f3a45ca17f15359
Max Dudin <dudin.mv@gmail.com>
2020-03-09 18:18:26 +0200

    K8SPXC-167 Update users package

--- pkg/pxc/users/users.go
+++ pkg/pxc/users/users.go
@@ -8,9 +8,6 @@ import (
 )
 
 func Job(cr *api.PerconaXtraDBCluster) *batchv1.Job {
-	/*labels := make(map[string]string)
-	labels["type"] = "usermanager"
-	labels["cluster"] = cr.Name*/
 	return &batchv1.Job{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "batch/v1",
@@ -19,7 +16,6 @@ func Job(cr *api.PerconaXtraDBCluster) *batchv1.Job {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "test",
 			Namespace: cr.Namespace,
-			//Labels:    labels,
 		},
 	}
 }
@@ -28,37 +24,32 @@ func JobSpec(rootPass string, conns string, job *batchv1.Job) batchv1.JobSpec {
 	backbackoffLimit := int32(1)
 	return batchv1.JobSpec{
 		BackoffLimit: &backbackoffLimit,
-		Selector:     &metav1.LabelSelector{
-			//MatchLabels: job.Labels,
-		},
 		Template: corev1.PodTemplateSpec{
-			ObjectMeta: metav1.ObjectMeta{
-				//Labels: job.Labels,
-			},
 			Spec: corev1.PodSpec{
 				RestartPolicy: corev1.RestartPolicyNever,
 				Containers: []corev1.Container{
 					{
 						Name:            "pxcusers",
-						Image:           "nonemax/users:latest",
+						Image:           "perconalab/percona-xtradb-cluster-operator",
 						ImagePullPolicy: corev1.PullAlways,
 						VolumeMounts: []corev1.VolumeMount{
 							{
-								MountPath: "/go/src/github.com/percona/pxcusers",
+								MountPath: "/",
 								Name:      "userssecret",
 								ReadOnly:  true,
 							},
 						},
 						Env: []corev1.EnvVar{
 							{
-								Name:  "PXC_CONNS",
+								Name:  "PXC_SERVICE",
 								Value: conns,
 							},
 							{
-								Name:  "PXC-ROOT-PASS",
+								Name:  "MYSQL_ROOT_PASSWORD",
 								Value: rootPass,
 							},
 						},
+						Command: []string{"user-manager"},
 					},
 				},
 				Volumes: []corev1.Volume{

d8ac59c602202ddd9e821806383ec4935b868705
AlyHKafoury <aly.kafoury@gmail.com>
2020-06-18 17:47:14 +0200

    adding haproxy service creator

--- pkg/pxc/service.go
+++ pkg/pxc/service.go
@@ -154,3 +154,45 @@ func NewServiceProxySQL(cr *api.PerconaXtraDBCluster) *corev1.Service {
 
 	return obj
 }
+
+func NewServiceHAProxy(cr *api.PerconaXtraDBCluster) *corev1.Service {
+	svcType := corev1.ServiceTypeClusterIP
+	if len(cr.Spec.HAProxy.ServiceType) > 0 {
+		svcType = cr.Spec.HAProxy.ServiceType
+	}
+	obj := &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Service",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-haproxy",
+			Namespace: cr.Namespace,
+			Labels: map[string]string{
+				"app.kubernetes.io/name":       "percona-xtradb-cluster",
+				"app.kubernetes.io/instance":   cr.Name,
+				"app.kubernetes.io/component":  "haproxy",
+				"app.kubernetes.io/managed-by": "percona-xtradb-cluster-operator",
+				"app.kubernetes.io/part-of":    "percona-xtradb-cluster",
+			},
+			Annotations: cr.Spec.HAProxy.ServiceAnnotations,
+		},
+		Spec: corev1.ServiceSpec{
+			Type: svcType,
+			Ports: []corev1.ServicePort{
+				{
+					Port: 3306,
+					Name: "mysql",
+				},
+			},
+			Selector: map[string]string{
+				"app.kubernetes.io/name":      "percona-xtradb-cluster",
+				"app.kubernetes.io/instance":  cr.Name,
+				"app.kubernetes.io/component": "haproxy",
+			},
+			LoadBalancerSourceRanges: cr.Spec.HAProxy.LoadBalancerSourceRanges,
+		},
+	}
+
+	return obj
+}

d41d042c158adbd6e516f1794da1ab3415685ced
Mykola Marzhan <mykola.marzhan@percona.com>
2020-07-06 15:23:59 +0300

    K8SPXC-344 fix 5.7 SST credentials

--- build/pxc-configure-pxc.sh
+++ build/pxc-configure-pxc.sh
@@ -65,6 +65,7 @@ sed -r "s|^[#]?wsrep_cluster_name=.*$|wsrep_cluster_name=${CLUSTER_NAME}|" ${CFG
 sed -r "s|^[#]?wsrep_sst_donor=.*$|wsrep_sst_donor=${DONOR_ADDRESS}|" ${CFG} 1<> ${CFG}
 sed -r "s|^[#]?wsrep_cluster_address=.*$|wsrep_cluster_address=gcomm://${WSREP_CLUSTER_ADDRESS}|" ${CFG} 1<> ${CFG}
 sed -r "s|^[#]?wsrep_node_incoming_address=.*$|wsrep_node_incoming_address=${NODE_NAME}:${NODE_PORT}|" ${CFG} 1<> ${CFG}
+sed -r "s|^[#]?wsrep_sst_auth=.*$|wsrep_sst_auth='xtrabackup:$XTRABACKUP_PASSWORD'|" ${CFG} 1<> ${CFG}
 
 CA=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 if [ -f /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt ]; then

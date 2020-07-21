0c25b2fa9eee3b9e70ee5f4267eda27149f82a62
Dmitriy Kostiuk <dmitriykostiuk@gmail.com>
2020-07-21 16:38:01 +0300

    RN, initial version (#440)
    * RN, initial version
    
    * update based on jira-bot
    
    * update on RN
    
    * remove bugfix which not affect users
    
    * fix descriptions
    
    * Update source/ReleaseNotes/Kubernetes-Operator-for-PXC-RN1.5.0.rst
    
    Co-authored-by: Mykola Marzhan <mykola.marzhan@percona.com>
    
    * Update source/ReleaseNotes/Kubernetes-Operator-for-PXC-RN1.5.0.rst
    
    Co-authored-by: Mykola Marzhan <mykola.marzhan@percona.com>
    
    * Update source/ReleaseNotes/Kubernetes-Operator-for-PXC-RN1.5.0.rst
    
    Co-authored-by: Mykola Marzhan <mykola.marzhan@percona.com>
    
    * Update source/ReleaseNotes/Kubernetes-Operator-for-PXC-RN1.5.0.rst
    
    Co-authored-by: Mykola Marzhan <mykola.marzhan@percona.com>
    
    * Update source/ReleaseNotes/Kubernetes-Operator-for-PXC-RN1.5.0.rst
    
    Co-authored-by: Mykola Marzhan <mykola.marzhan@percona.com>
    
    * Update source/ReleaseNotes/Kubernetes-Operator-for-PXC-RN1.5.0.rst
    
    Co-authored-by: Mykola Marzhan <mykola.marzhan@percona.com>
    
    * update based on comments
    
    * Update source/ReleaseNotes/Kubernetes-Operator-for-PXC-RN1.5.0.rst
    
    Co-authored-by: Mykola Marzhan <mykola.marzhan@percona.com>
    
    * Update source/ReleaseNotes/Kubernetes-Operator-for-PXC-RN1.5.0.rst
    
    Co-authored-by: Mykola Marzhan <mykola.marzhan@percona.com>
    
    * more title fixes
    
    * one more title fix
    
    * minor fix
    
    * K8SPXC-356 - update Release Notes
    
    Co-authored-by: Mykola Marzhan <mykola.marzhan@percona.com>

--- source/conf.py
+++ source/conf.py
@@ -37,9 +37,9 @@ project = u'Percona Kubernetes Operator for PXC'
 copyright = u'Percona LLC and/or its affiliates 2009 - 2020'
 
 # the short X.Y version
-version = '1.4.0'
+version = '1.5.0'
 # the full version including alpha/beta/rc tags.
-release = '1.4.0'
+release = '1.5.0'
 
 
 # List of patterns, relative to source directory, that match files and

666023f10be03ce2192dbf28a74773c0523b2715
Tomislav Plavcic <tomislav.plavcic@percona.com>
2020-09-21 10:16:53 +0200

    Fix install of peer-list

--- build/pxc-init-entrypoint.sh
+++ build/pxc-init-entrypoint.sh
@@ -8,4 +8,4 @@ install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /unsafe-bootstrap.sh /var/lib/mys
 install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /pxc-configure-pxc.sh /var/lib/mysql/pxc-configure-pxc.sh
 install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /liveness-check.sh /var/lib/mysql/liveness-check.sh
 install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /readiness-check.sh /var/lib/mysql/readiness-check.sh
-install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /usr/local/bin/peer-list /usr/local/bin/peer-list
+install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /peer-list /usr/local/bin/peer-list

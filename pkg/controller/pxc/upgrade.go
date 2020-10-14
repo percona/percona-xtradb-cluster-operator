d9db2f5844dc8e827e8ab8b151f11730c2d32be2
Paval Kasko <pavel.kasko@percona.com>
2020-10-14 11:58:10 +0300

    Improve smart-update limit logic (#577)
    it has a more fair done timer and retries period.
    it also doesn't have an extra tail wait after the limit is reached.
    and action runs first time instantly
    
    Co-authored-by: Andrew Pogrebnoy <absourd.noise@gmail.com>

--- pkg/controller/pxc/upgrade.go
+++ pkg/controller/pxc/upgrade.go
@@ -288,31 +288,55 @@ func (r *ReconcilePerconaXtraDBCluster) waitUntilOnline(cr *api.PerconaXtraDBClu
 
 	podNamePrefix := fmt.Sprintf("%s.%s.%s", pod.Name, sfsName, cr.Namespace)
 
-	for i := 0; i < waitLimit; i++ {
-		statuses, err := database.Status(podNamePrefix, pod.Status.PodIP)
-		if err != nil && err != queries.ErrNotFound {
-			return fmt.Errorf("failed to get status: %v", err)
-		}
-
-		online := false
-		for _, status := range statuses {
-			if status == "ONLINE" {
-				online = true
-			} else {
-				online = false
-				break
+	return retry(time.Second*10, time.Duration(waitLimit)*time.Second,
+		func() (bool, error) {
+			statuses, err := database.Status(podNamePrefix, pod.Status.PodIP)
+			if err != nil && err != queries.ErrNotFound {
+				return false, fmt.Errorf("failed to get status: %v", err)
 			}
-		}
 
-		if online {
+			for _, status := range statuses {
+				if status != "ONLINE" {
+					return false, nil
+				}
+			}
+
 			log.Info(fmt.Sprintf("pod %s is online", pod.Name))
-			return nil
-		}
+			return true, nil
+		})
+}
 
-		time.Sleep(time.Second * 1)
+// retry runs func "f" every "in" time until "limit" is reached
+// it also doesn't have an extra tail wait after the limit is reached
+// and f func runs first time instantly
+func retry(in, limit time.Duration, f func() (bool, error)) error {
+	fdone, err := f()
+	if err != nil {
+		return err
+	}
+	if fdone {
+		return nil
 	}
 
-	return fmt.Errorf("reach pod wait limit")
+	done := time.NewTimer(limit)
+	defer done.Stop()
+	tk := time.NewTicker(in)
+	defer tk.Stop()
+
+	for {
+		select {
+		case <-done.C:
+			return fmt.Errorf("reach pod wait limit")
+		case <-tk.C:
+			fdone, err := f()
+			if err != nil {
+				return err
+			}
+			if fdone {
+				return nil
+			}
+		}
+	}
 }
 
 func (r *ReconcilePerconaXtraDBCluster) proxyDB(cr *api.PerconaXtraDBCluster) (queries.Database, error) {
@@ -398,49 +422,47 @@ func (r *ReconcilePerconaXtraDBCluster) waitPXCSynced(cr *api.PerconaXtraDBClust
 
 	defer database.Close()
 
-	for i := 0; i < waitLimit; i++ {
-		state, err := database.WsrepLocalStateComment()
-		if err != nil {
-			return fmt.Errorf("failed to get wsrep local state: %v", err)
-		}
+	return retry(time.Second*10, time.Duration(waitLimit)*time.Second,
+		func() (bool, error) {
+			state, err := database.WsrepLocalStateComment()
+			if err != nil {
+				return false, fmt.Errorf("failed to get wsrep local state: %v", err)
+			}
 
-		if state == "Synced" {
-			return nil
-		}
+			if state == "Synced" {
+				return true, nil
+			}
 
-		time.Sleep(time.Second * 1)
-	}
-
-	return fmt.Errorf("reach pod wait limit")
+			return false, nil
+		})
 }
 
 func (r *ReconcilePerconaXtraDBCluster) waitPodRestart(updateRevision string, pod *corev1.Pod, waitLimit int) error {
-	for i := 0; i < waitLimit; i++ {
-		time.Sleep(time.Second * 1)
-
-		err := r.client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, pod)
-		if err != nil && !k8serrors.IsNotFound(err) {
-			return err
-		}
-
-		ready := false
-		for _, container := range pod.Status.ContainerStatuses {
-			if container.Name == "pxc" {
-				ready = container.Ready
+	return retry(time.Second*10, time.Duration(waitLimit)*time.Second,
+		func() (bool, error) {
+			err := r.client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, pod)
+			if err != nil && !k8serrors.IsNotFound(err) {
+				return false, err
 			}
-		}
 
-		if pod.Status.Phase == corev1.PodFailed {
-			return errors.Errorf("pod %s is in failed phase", pod.Name)
-		}
+			ready := false
+			for _, container := range pod.Status.ContainerStatuses {
+				if container.Name == "pxc" {
+					ready = container.Ready
+				}
+			}
 
-		if pod.Status.Phase == corev1.PodRunning && pod.ObjectMeta.Labels["controller-revision-hash"] == updateRevision && ready {
-			log.Info(fmt.Sprintf("pod %s is running", pod.Name))
-			return nil
-		}
-	}
+			if pod.Status.Phase == corev1.PodFailed {
+				return false, errors.Errorf("pod %s is in failed phase", pod.Name)
+			}
 
-	return fmt.Errorf("reach pod wait limit")
+			if pod.Status.Phase == corev1.PodRunning && pod.ObjectMeta.Labels["controller-revision-hash"] == updateRevision && ready {
+				log.Info(fmt.Sprintf("pod %s is running", pod.Name))
+				return true, nil
+			}
+
+			return false, nil
+		})
 }
 
 func isPXC(sfs api.StatefulApp) bool {

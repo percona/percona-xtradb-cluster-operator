dc6928eca41beb700113c7375cbaf31c41ced2fe
Tomislav Plavcic <tomislav.plavcic@percona.com>
2021-12-28 23:38:23 +0100

    Fix self-healing-chaos for new PXC versions

--- e2e-tests/self-healing-chaos/compare/proxy-vars.sql
+++ e2e-tests/self-healing-chaos/compare/proxy-vars.sql
@@ -143,7 +143,7 @@ mysql-query_processor_iterations	0
 mysql-query_retries_on_failure	1
 mysql-reset_connection_algorithm	2
 mysql-server_capabilities	571947
-mysql-server_version	5.7.34
+mysql-server_version	5.7.35
 mysql-servers_stats	true
 mysql-session_idle_ms	1000
 mysql-session_idle_show_processlist	true

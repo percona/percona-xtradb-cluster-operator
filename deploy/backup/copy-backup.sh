9458603e4e175b282f9523f37892358dbc1f3e3e
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2022-06-17 15:32:32 +0300

    Update deploy/backup/copy-backup.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- deploy/backup/copy-backup.sh
+++ deploy/backup/copy-backup.sh
@@ -110,7 +110,7 @@ check_input_destination() {
 	elif [ "${backup_dest:0:5}" = "s3://" ]; then
 		env -i $CREDENTIALS "$xbcloud" get "${backup_dest}" xtrabackup_info 1>/dev/null
 	elif [ "${backup_dest:0:8}" = "azure://" ]; then
-		env -i $AZURE_CREDENTIALS "$xbcloud" get "${backup_dest}" xtrabackup_info 1>/dev/null
+		env -i "$AZURE_CREDENTIALS" "$xbcloud" get "${backup_dest}" xtrabackup_info 1>/dev/null
 	else
 		echo "Can't find $backup_dest backup"
 		usage

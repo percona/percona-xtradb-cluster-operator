25056e7e3a6ae28c1885cb99d92c2407aa859ae8
Andrii Dema <a.dema@jazzserve.com>
2022-10-25 17:38:54 +0300

    K8SPXC-1065: add seconds to scheduled backups (#1233)
    https://jira.percona.com/browse/K8SPXC-1065
    
    This PR changes cron behaviour.
    Cronjob will be launched in the exact seconds when it was added.
    
    For example, if you create a cluster at the time 12:58:32 with the
    schedule `* * * * *` the next cron job will execute at 12:59:00.
    After PR changes, it will execute at 12:59:32
    
    Co-authored-by: Viacheslav Sarzhan <slava.sarzhan@percona.com>

--- pkg/controller/pxc/controller.go
+++ pkg/controller/pxc/controller.go
@@ -153,6 +153,17 @@ type CronRegistry struct {
 	backupJobs        *sync.Map
 }
 
+// AddFuncWithSeconds does the same as cron.AddFunc but changes the schedule so that the function will run the exact second that this method is called.
+func (r *CronRegistry) AddFuncWithSeconds(spec string, cmd func()) (cron.EntryID, error) {
+	schedule, err := cron.ParseStandard(spec)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to parse cron schedule")
+	}
+	schedule.(*cron.SpecSchedule).Second = uint64(1 << time.Now().Second())
+	id := r.crons.Schedule(schedule, cron.FuncJob(cmd))
+	return id, nil
+}
+
 type Schedule struct {
 	ID           int
 	CronSchedule string

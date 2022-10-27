a406f9476219cf9ee3916e0d17ae3612f5497fb1
Ege Güneş <ege.gunes@percona.com>
2022-10-27 17:15:45 +0300

    check if buffer_pool or max_connections already configured by user

--- pkg/pxc/app/config/autotune.go
+++ pkg/pxc/app/config/autotune.go
@@ -1,10 +1,12 @@
 package config
 
 import (
-	"errors"
 	"strconv"
 
+	"github.com/pkg/errors"
 	res "k8s.io/apimachinery/pkg/api/resource"
+
+	api "github.com/percona/percona-xtradb-cluster-operator/pkg/apis/pxc/v1"
 )
 
 const (
@@ -12,45 +14,59 @@ const (
 	chunkSizeDefault int64 = 134217728
 )
 
-func getAutoTuneParams(q *res.Quantity) (string, error) {
+func getAutoTuneParams(cr *api.PerconaXtraDBCluster, q *res.Quantity) (string, error) {
 	autotuneParams := ""
 
-	poolSize := q.Value() / int64(100) * int64(75)
-	if q.Value()-poolSize < int64(1000000000) {
-		poolSize = q.Value() / int64(100) * int64(50)
+	bufferConfigured, err := cr.ConfigHasKey("mysqld", "innodb_buffer_pool_size")
+	if err != nil {
+		return autotuneParams, errors.Wrap(err, "check if innodb_buffer_pool_size configured")
 	}
-	poolSize = poolSize + chunkSizeDefault - (poolSize % chunkSizeDefault)
-	poolSizeVal := strconv.FormatInt(poolSize, 10)
-	paramValue := "\n" + "innodb_buffer_pool_size" + " = " + poolSizeVal
-	autotuneParams += paramValue
 
-	// Adjust innodb_buffer_pool_chunk_size
-	// If innodb_buffer_pool_size is bigger than 1G, innodb_buffer_pool_instances is set to 8.
-	// By default, innodb_buffer_pool_chunk_size is 128M and innodb_buffer_pool_size needs to be
-	// multiple of innodb_buffer_pool_chunk_size * innodb_buffer_pool_instances.
-	// More info: https://dev.mysql.com/doc/refman/8.0/en/innodb-buffer-pool-resize.html
-	if poolSize > int64(1000000000) {
-		chunkSize := poolSize / 8
-		// round to multiple of chunkSizeMin
-		chunkSize = chunkSize + chunkSizeMin - (chunkSize % chunkSizeMin)
+	maxConnConfigured, err := cr.ConfigHasKey("mysqld", "max_connections")
+	if err != nil {
+		return autotuneParams, errors.Wrap(err, "check if max_connections configured")
+	}
 
-		poolSize = chunkSize * 8
+	if !bufferConfigured {
+		poolSize := q.Value() / int64(100) * int64(75)
+		if q.Value()-poolSize < int64(1000000000) {
+			poolSize = q.Value() / int64(100) * int64(50)
+		}
+		poolSize = poolSize + chunkSizeDefault - (poolSize % chunkSizeDefault)
 		poolSizeVal := strconv.FormatInt(poolSize, 10)
-		autotuneParams = "\n" + "innodb_buffer_pool_size" + " = " + poolSizeVal
+		paramValue := "\n" + "innodb_buffer_pool_size" + " = " + poolSizeVal
+		autotuneParams += paramValue
 
-		chunkSizeVal := strconv.FormatInt(chunkSize, 10)
-		paramValue = "\n" + "innodb_buffer_pool_chunk_size" + " = " + chunkSizeVal
+		// Adjust innodb_buffer_pool_chunk_size
+		// If innodb_buffer_pool_size is bigger than 1G, innodb_buffer_pool_instances is set to 8.
+		// By default, innodb_buffer_pool_chunk_size is 128M and innodb_buffer_pool_size needs to be
+		// multiple of innodb_buffer_pool_chunk_size * innodb_buffer_pool_instances.
+		// More info: https://dev.mysql.com/doc/refman/8.0/en/innodb-buffer-pool-resize.html
+		if poolSize > int64(1000000000) {
+			chunkSize := poolSize / 8
+			// round to multiple of chunkSizeMin
+			chunkSize = chunkSize + chunkSizeMin - (chunkSize % chunkSizeMin)
+
+			poolSize = chunkSize * 8
+			poolSizeVal := strconv.FormatInt(poolSize, 10)
+			autotuneParams = "\n" + "innodb_buffer_pool_size" + " = " + poolSizeVal
+
+			chunkSizeVal := strconv.FormatInt(chunkSize, 10)
+			paramValue = "\n" + "innodb_buffer_pool_chunk_size" + " = " + chunkSizeVal
+			autotuneParams += paramValue
+		}
+	}
+
+	if !maxConnConfigured {
+		divider := int64(12582880)
+		if q.Value() < divider {
+			return "", errors.New("Not enough memory set in requests. Must be >= 12Mi.")
+		}
+		maxConnSize := q.Value() / divider
+		maxConnSizeVal := strconv.FormatInt(maxConnSize, 10)
+		paramValue := "\n" + "max_connections" + " = " + maxConnSizeVal
 		autotuneParams += paramValue
 	}
-
-	divider := int64(12582880)
-	if q.Value() < divider {
-		return "", errors.New("Not enough memory set in requests. Must be >= 12Mi.")
-	}
-	maxConnSize := q.Value() / divider
-	maxConnSizeVal := strconv.FormatInt(maxConnSize, 10)
-	paramValue = "\n" + "max_connections" + " = " + maxConnSizeVal
-	autotuneParams += paramValue
 
 	return autotuneParams, nil
 }

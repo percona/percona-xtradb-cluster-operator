5ca289bb6e8c3f225ffa9b64480bb10f5ff97cae
Andrii Dema <a.dema@jazzserve.com>
2022-11-04 17:38:09 +0200

    K8SPXC-1082: add `ignoreAnnotations` and `ignoreLabels` options for services (#1257)
    https://jira.percona.com/browse/K8SPXC-1082
    
    Co-authored-by: Viacheslav Sarzhan <slava.sarzhan@percona.com>

--- pkg/controller/pxc/replication.go
+++ pkg/controller/pxc/replication.go
@@ -55,7 +55,7 @@ func (r *ReconcilePerconaXtraDBCluster) ensurePxcPodServices(cr *api.PerconaXtra
 			return errors.Wrap(err, "failed to set owner to external service")
 		}
 
-		err = r.createOrUpdate(svc)
+		err = r.createOrUpdate(cr, svc)
 		if err != nil {
 			return errors.Wrap(err, "failed to ensure pxc service")
 		}

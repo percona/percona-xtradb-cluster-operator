c076ab705a7d16dd038a8f11be1e492c6cca40fe
Inel Pandzic <inel.pandzic@percona.com>
2023-03-27 18:30:11 +0200

    Update pkg/controller/pxc/backup.go
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkg/controller/pxc/backup.go
+++ pkg/controller/pxc/backup.go
@@ -19,7 +19,6 @@ import (
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 

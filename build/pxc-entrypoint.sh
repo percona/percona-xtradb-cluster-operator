252885446eae069689082addf4525806a1305844
Inel Pandzic <inel.pandzic@percona.com>
2023-04-25 14:16:26 +0200

    Grants for all hosts.

--- build/pxc-entrypoint.sh
+++ build/pxc-entrypoint.sh
@@ -386,10 +386,10 @@ if [ -z "$CLUSTER_JOIN" ] && [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 
 			CREATE USER 'xtrabackup'@'%' IDENTIFIED BY '$(escape_special "${XTRABACKUP_PASSWORD}")';
 
-			GRANT BACKUP_ADMIN, PROCESS, RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'xtrabackup'@'localhost';
-			GRANT SELECT ON performance_schema.log_status TO 'xtrabackup'@'localhost';
-			GRANT SELECT ON performance_schema.keyring_component_status TO 'xtrabackup@'localhost';
-			GRANT SELECT ON performance_schema.replication_group_members TO 'xtrabackup@'localhost';
+			GRANT BACKUP_ADMIN, PROCESS, RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'xtrabackup'@'%';
+			GRANT SELECT ON performance_schema.log_status TO 'xtrabackup'@'%';
+			GRANT SELECT ON performance_schema.keyring_component_status TO 'xtrabackup@'%';
+			GRANT SELECT ON performance_schema.replication_group_members TO 'xtrabackup@'%';
 
 			CREATE USER 'monitor'@'${MONITOR_HOST}' IDENTIFIED BY '$(escape_special "${MONITOR_PASSWORD}")' WITH MAX_USER_CONNECTIONS 100;
 			GRANT SELECT, PROCESS, SUPER, REPLICATION CLIENT, RELOAD ON *.* TO 'monitor'@'${MONITOR_HOST}';

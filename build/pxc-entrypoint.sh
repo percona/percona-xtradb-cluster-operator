17cfe2a5a6779ba0b83f5ef768d4c85ab7861aa1
Inel Pandzic <inel.pandzic@percona.com>
2023-04-27 16:24:44 +0200

    Remove grants for replication_group_members table.

--- build/pxc-entrypoint.sh
+++ build/pxc-entrypoint.sh
@@ -388,7 +388,6 @@ if [ -z "$CLUSTER_JOIN" ] && [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 			GRANT BACKUP_ADMIN, RESOURCE_GROUP_USER, SERVICE_CONNECTION_ADMIN, PROCESS, RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'xtrabackup'@'%';
 			GRANT SELECT ON performance_schema.log_status TO 'xtrabackup'@'%';
 			GRANT SELECT ON performance_schema.keyring_component_status TO 'xtrabackup'@'%';
-			GRANT SELECT ON performance_schema.replication_group_members TO 'xtrabackup'@'%';
 
 			CREATE USER 'monitor'@'${MONITOR_HOST}' IDENTIFIED BY '$(escape_special "${MONITOR_PASSWORD}")' WITH MAX_USER_CONNECTIONS 100;
 			GRANT SELECT, PROCESS, SUPER, REPLICATION CLIENT, RELOAD ON *.* TO 'monitor'@'${MONITOR_HOST}';

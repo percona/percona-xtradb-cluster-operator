fa692c456c0a365c84bfe53bbe8ae3fefc94a6fd
Andrii Dema <a.dema@jazzserve.com>
2023-12-13 21:16:37 +0200

    K8SPXC-1298: add prefix for pmm cluster name
    https://jira.percona.com/browse/K8SPXC-1298

--- build/pmm-prerun.sh
+++ build/pmm-prerun.sh
@@ -30,6 +30,8 @@ if [[ $DB_TYPE == "haproxy" ]]; then
 	)
 fi
 
+CLUSTER_NAME="${PMM_PREFIX}${CLUSTER_NAME}"
+
 pmm-admin status --wait=10s
 pmm-admin add "$DB_TYPE" --skip-connection-check --metrics-mode=push --username="$DB_USER" --password="$DB_PASSWORD" --cluster="$CLUSTER_NAME" "${pmm_args[@]}"
 pmm-admin annotate --service-name="$PMM_AGENT_SETUP_NODE_NAME" 'Service restarted'

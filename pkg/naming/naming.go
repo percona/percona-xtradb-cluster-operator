fb465b95d2802ccde160d0508dfec24fc62cb83d
Natalia Marukovich <natalia.marukovich@percona.com>
2024-07-05 14:41:40 +0200

    delete unused

--- pkg/naming/naming.go
+++ pkg/naming/naming.go
@@ -1,22 +1,8 @@
 package naming
 
 const (
-	annotationPrefix      = "percona.com/"
-	annotationPrefixMysql = "pxc.percona.com/"
+	annotationPrefix = "percona.com/"
 )
-
-//const (
-//	LabelName      = "app.kubernetes.io/name"
-//	LabelInstance  = "app.kubernetes.io/instance"
-//	LabelManagedBy = "app.kubernetes.io/managed-by"
-//	LabelPartOf    = "app.kubernetes.io/part-of"
-//	LabelComponent = "app.kubernetes.io/component"
-//)
-//
-//const (
-//	LabelMySQLPrimary = annotationPrefixMysql + "primary"
-//	LabelExposed      = annotationPrefix + "exposed"
-//)
 
 const (
 	FinalizerDeleteSSL            = annotationPrefix + "delete-ssl"
@@ -26,17 +12,3 @@ const (
 
 	FinalizerDeleteS3Backup = annotationPrefix + "delete-s3-backup" // TODO: rename to a more appropriate name like `delete-backup`
 )
-
-//type AnnotationKey string
-//
-//func (s AnnotationKey) String() string {
-//	return string(s)
-//}
-//
-//const (
-//	AnnotationSecretHash       AnnotationKey = annotationPrefix + "last-applied-secret"
-//	AnnotationConfigHash       AnnotationKey = annotationPrefix + "configuration-hash"
-//	AnnotationTLSHash          AnnotationKey = annotationPrefix + "last-applied-tls"
-//	AnnotationPasswordsUpdated AnnotationKey = annotationPrefix + "passwords-updated"
-//	AnnotationLastConfigHash   AnnotationKey = annotationPrefix + "last-config-hash"
-//)

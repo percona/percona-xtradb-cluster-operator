1f51f1ade9d730c8a3e6e9d7b752ae40a0a31a69
Inel Pandzic <inel.pandzic@percona.com>
2024-07-08 17:35:23 +0200

    Fix script paths.

--- build/haproxy-init-entrypoint.sh
+++ build/haproxy-init-entrypoint.sh
@@ -3,9 +3,9 @@
 set -o errexit
 set -o xtrace
 
-install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /check_pxc.sh /usr/local/bin/check_pxc.sh
-install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /add_pxc_nodes.sh /usr/bin/add_pxc_nodes.sh
-install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /readiness-check.sh /usr/local/bin/readiness-check.sh
-install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /liveness-check.sh /usr/local/bin/liveness-check.sh
-install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /haproxy.cfg /etc/haproxy/haproxy.cfg
-install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /haproxy-global.cfg /etc/haproxy/haproxy-global.cfg
+install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /haproxy/check_pxc.sh /usr/local/bin/check_pxc.sh
+install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /haproxy/add_pxc_nodes.sh /usr/bin/add_pxc_nodes.sh
+install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /haproxy/readiness-check.sh /usr/local/bin/readiness-check.sh
+install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /haproxy/liveness-check.sh /usr/local/bin/liveness-check.sh
+install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg
+install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /haproxy/haproxy-global.cfg /etc/haproxy/haproxy-global.cfg

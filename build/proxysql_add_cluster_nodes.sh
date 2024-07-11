f8b46ca9325991d5e57e8858f124446be5061eea
Inel Pandzic <inel.pandzic@percona.com>
2024-07-11 11:20:39 +0200

    Update build/proxysql_add_cluster_nodes.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- build/proxysql_add_cluster_nodes.sh
+++ build/proxysql_add_cluster_nodes.sh
@@ -2,9 +2,9 @@
 
 ipaddr=$(hostname -I | awk ' { print $1 } ')
 
-for i in $(curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/ | jq -r '.node.nodes[]?.key' | awk -F'/' '{print $(NF)}'); do
-	echo $i
-	mysql -h $i -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL ON *.* TO '$MYSQL_PROXY_USER'@'$ipaddr' IDENTIFIED BY '$MYSQL_PROXY_PASSWORD'"
+for i in $(curl http://"$DISCOVERY_SERVICE"/v2/keys/pxc-cluster/"$CLUSTER_NAME"/ | jq -r '.node.nodes[]?.key' | awk -F'/' '{print $(NF)}'); do
+	echo "$i"
+	mysql -h "$i" -uroot -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO '$MYSQL_PROXY_USER'@'$ipaddr' IDENTIFIED BY '$MYSQL_PROXY_PASSWORD'"
 	mysql -h 127.0.0.1 -P6032 -uadmin -padmin -e "INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (0, '$i', 3306, 20);"
 done
 

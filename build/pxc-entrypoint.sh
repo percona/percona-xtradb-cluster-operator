0189cce8cb138ef44af8b95b675087667c8995ee
Inel Pandzic <inel.pandzic@percona.com>
2024-07-11 11:17:13 +0200

    Update build/pxc-entrypoint.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- build/pxc-entrypoint.sh
+++ build/pxc-entrypoint.sh
@@ -58,21 +58,21 @@ process_init_file() {
 	local mysql=("$@")
 
 	case "$f" in
-	*.sh)
-		echo "$0: running $f"
-		. "$f"
-		;;
-	*.sql)
-		echo "$0: running $f"
-		"${mysql[@]}" <"$f"
-		echo
-		;;
-	*.sql.gz)
-		echo "$0: running $f"
-		gunzip -c "$f" | "${mysql[@]}"
-		echo
-		;;
-	*) echo "$0: ignoring $f" ;;
+		*.sh)
+			echo "$0: running $f"
+			. "$f"
+			;;
+		*.sql)
+			echo "$0: running $f"
+			"${mysql[@]}" <"$f"
+			echo
+			;;
+		*.sql.gz)
+			echo "$0: running $f"
+			gunzip -c "$f" | "${mysql[@]}"
+			echo
+			;;
+		*) echo "$0: ignoring $f" ;;
 	esac
 	echo
 }

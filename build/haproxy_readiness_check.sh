b9762804d4743f66b15cb9ddf0ae83ae51504cdb
Inel Pandzic <inel.pandzic@percona.com>
2024-07-11 11:16:24 +0200

    Update build/haproxy_readiness_check.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- build/haproxy_readiness_check.sh
+++ build/haproxy_readiness_check.sh
@@ -10,7 +10,7 @@ export MYSQL_PWD=$(cat /etc/mysql/mysql-users-secret/monitor)
 
 STATUS=$($MYSQL_CMDLINE -h127.0.0.1 -P$PXC_SERVER_PORT -e 'select 1;' | sed -n -e '2p' | tr '\n' ' ')
 
-if [[ "${STATUS}" -eq 1 ]]; then
+if [[ ${STATUS} -eq 1 ]]; then
 	exit 0
 fi
 

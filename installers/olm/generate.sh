67d040ba74e5b30dce49304e5032226ea90d1a2d
Natalia Marukovich <nmarukovich@gmail.com>
2024-10-16 18:32:01 +0300

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- installers/olm/generate.sh
+++ installers/olm/generate.sh
@@ -22,12 +22,12 @@ project_name='percona-xtradb-cluster-operator'
 # https://github.com/redhat-openshift-ecosystem/certification-releases/blob/main/4.9/ga/troubleshooting.md#get-supported-versions
 file_name='percona-xtradb-cluster-operator'
 
-kubectl kustomize "../../config/${DISTRIBUTION}" > operator_yamls.yaml
+kubectl kustomize "../../config/${DISTRIBUTION}" >operator_yamls.yaml
 
-yq eval '. | select(.kind == "CustomResourceDefinition")' operator_yamls.yaml > operator_crds.yaml
-yq eval '. | select(.kind == "Deployment")' operator_yamls.yaml > operator_deployments.yaml
-yq eval '. | select(.kind == "ServiceAccount")' operator_yamls.yaml > operator_accounts.yaml
-yq eval '. | select(.kind == "Role")' operator_yamls.yaml > operator_roles.yaml
+yq eval '. | select(.kind == "CustomResourceDefinition")' operator_yamls.yaml >operator_crds.yaml
+yq eval '. | select(.kind == "Deployment")' operator_yamls.yaml >operator_deployments.yaml
+yq eval '. | select(.kind == "ServiceAccount")' operator_yamls.yaml >operator_accounts.yaml
+yq eval '. | select(.kind == "Role")' operator_yamls.yaml >operator_roles.yaml
 
 ## Recreate the Operator SDK project.
 
@@ -39,9 +39,9 @@ install -d "${project_directory}"
 
 	# Generate CRD descriptions from Go markers.
 	# https://sdk.operatorframework.io/docs/building-operators/golang/references/markers/
-    yq eval '[. | {"group": .spec.group, "kind": .spec.names.kind, "version": .spec.versions[].name}]' ../../operator_crds.yaml > crd_gvks.yaml
+	yq eval '[. | {"group": .spec.group, "kind": .spec.names.kind, "version": .spec.versions[].name}]' ../../operator_crds.yaml >crd_gvks.yaml
 
-    yq eval --inplace '.multigroup = true | .resources = load("crd_gvks.yaml" | fromyaml) | .' ./PROJECT
+	yq eval --inplace '.multigroup = true | .resources = load("crd_gvks.yaml" | fromyaml) | .' ./PROJECT
 
 	ln -s "${go_api_directory}" .
 	operator-sdk generate kustomize manifests --interactive='false' --verbose
@@ -51,7 +51,7 @@ install -d "${project_directory}"
 [ ! -d "${bundle_directory}" ] || rm -r "${bundle_directory}"
 install -d \
 	"${bundle_directory}/manifests" \
-	"${bundle_directory}/metadata" \
+	"${bundle_directory}/metadata"
 
 # `echo "${operator_yamls}" | operator-sdk generate bundle` includes the ServiceAccount which cannot
 # be upgraded: https://github.com/operator-framework/operator-lifecycle-manager/issues/2193
@@ -68,54 +68,57 @@ export openshift_supported_versions="${OPENSHIFT_VERSIONS}"
 yq eval '.annotations["operators.operatorframework.io.bundle.channels.v1"] = $package_channel |
          .annotations["operators.operatorframework.io.bundle.channel.default.v1"] = $package_channel |
          .annotations["com.redhat.openshift.versions"] = env(openshift_supported_versions)' \
-	bundle.annotations.yaml > "${bundle_directory}/metadata/annotations.yaml"
+	bundle.annotations.yaml >"${bundle_directory}/metadata/annotations.yaml"
 
 if [ ${DISTRIBUTION} == 'community' ]; then
-# community-operators
-yq eval '.annotations["operators.operatorframework.io.bundle.package.v1"] = "percona-xtradb-cluster-operator" |
+	# community-operators
+	yq eval '.annotations["operators.operatorframework.io.bundle.package.v1"] = "percona-xtradb-cluster-operator" |
          .annotations["org.opencontainers.image.authors"] = "info@percona.com" |
          .annotations["org.opencontainers.image.url"] = "https://percona.com" |
          .annotations["org.opencontainers.image.vendor"] = "Percona"' \
-	bundle.annotations.yaml > "${bundle_directory}/metadata/annotations.yaml"
+		bundle.annotations.yaml >"${bundle_directory}/metadata/annotations.yaml"
 
 # certified-operators
-elif [ ${DISTRIBUTION} == 'redhat' ];then
-yq eval --inplace '
+elif [ ${DISTRIBUTION} == 'redhat' ]; then
+	yq eval --inplace '
     .annotations["operators.operatorframework.io.bundle.package.v1"] = "percona-xtradb-cluster-operator-certified" ' \
-	"${bundle_directory}/metadata/annotations.yaml"
+		"${bundle_directory}/metadata/annotations.yaml"
 
 # redhat-marketplace
-elif [ ${DISTRIBUTION} == 'marketplace' ];then
-yq eval --inplace '
+elif [ ${DISTRIBUTION} == 'marketplace' ]; then
+	yq eval --inplace '
     .annotations["operators.operatorframework.io.bundle.package.v1"] = "percona-xtradb-cluster-operator-certified-rhmp" ' \
-	"${bundle_directory}/metadata/annotations.yaml"
+		"${bundle_directory}/metadata/annotations.yaml"
 fi
 
 # Copy annotations into Dockerfile LABELs.
 # TODO fix tab for labels.
 
 labels=$(yq eval -r '.annotations | to_entries | map("    " + .key + "=" + (.value | tojson)) | join("\n")' \
-"${bundle_directory}/metadata/annotations.yaml")
+	"${bundle_directory}/metadata/annotations.yaml")
 
-ANNOTATIONS="${labels}" envsubst < bundle.Dockerfile > "${bundle_directory}/Dockerfile"
+ANNOTATIONS="${labels}" envsubst <bundle.Dockerfile >"${bundle_directory}/Dockerfile"
 
 # Include CRDs as manifests.
 crd_names=$(yq eval -o=tsv '.metadata.name' operator_crds.yaml)
 
 for name in ${crd_names}; do
-    yq eval ". | select(.metadata.name == \"${name}\")" operator_crds.yaml > "${bundle_directory}/manifests/${name}.crd.yaml"
+	yq eval ". | select(.metadata.name == \"${name}\")" operator_crds.yaml >"${bundle_directory}/manifests/${name}.crd.yaml"
 done
 
-abort() { echo >&2 "$@"; exit 1; }
+abort() {
+	echo >&2 "$@"
+	exit 1
+}
 dump() { yq --color-output; }
 
 # The first command render yaml correctly and the second extract data.
 
-yq eval -i '[.]' operator_deployments.yaml && yq eval 'length == 1' operator_deployments.yaml --exit-status > /dev/null || abort "too many deployments accounts!" $'\n'"$(yq eval . operator_deployments.yaml)"
+yq eval -i '[.]' operator_deployments.yaml && yq eval 'length == 1' operator_deployments.yaml --exit-status >/dev/null || abort "too many deployments accounts!" $'\n'"$(yq eval . operator_deployments.yaml)"
 
-yq eval -i '[.]' operator_accounts.yaml && yq eval 'length == 1' operator_accounts.yaml --exit-status > /dev/null || abort "too many service accounts!" $'\n'"$(yq eval . operator_accounts.yaml)"
+yq eval -i '[.]' operator_accounts.yaml && yq eval 'length == 1' operator_accounts.yaml --exit-status >/dev/null || abort "too many service accounts!" $'\n'"$(yq eval . operator_accounts.yaml)"
 
-yq eval -i '[.]' operator_roles.yaml && yq eval 'length == 1' operator_roles.yaml --exit-status > /dev/null || abort "too many roles!" $'\n'"$(yq eval . operator_roles.yaml)"
+yq eval -i '[.]' operator_roles.yaml && yq eval 'length == 1' operator_roles.yaml --exit-status >/dev/null || abort "too many roles!" $'\n'"$(yq eval . operator_roles.yaml)"
 
 # Render bundle CSV and strip comments.
 csv_stem=$(yq -r '.projectName' "${project_directory}/PROJECT")
@@ -147,21 +150,21 @@ yq eval '
   .spec.version = env(version) |
   .spec.install.spec.permissions = [{ "serviceAccountName": env(account), "rules": env(rules) }] |
   .spec.install.spec.deployments = [( env(deployment) | .[] |{ "name": .metadata.name, "spec": .spec} )] |
-  .spec.minKubeVersion = env(minKubeVer)'  bundle.csv.yaml > "${bundle_directory}/manifests/${file_name}.clusterserviceversion.yaml"
+  .spec.minKubeVersion = env(minKubeVer)' bundle.csv.yaml >"${bundle_directory}/manifests/${file_name}.clusterserviceversion.yaml"
 
-if [[ "${DISTRIBUTION}" == "redhat" ]];then
-echo "REDHAT"
-    yq eval --inplace '
+if [[ ${DISTRIBUTION} == "redhat" ]]; then
+	echo "REDHAT"
+	yq eval --inplace '
         .spec.relatedImages = env(relatedImages) |
         .metadata.annotations.certified = "true" |
         .metadata.annotations["containerImage"] = "registry.connect.redhat.com/percona/percona-xtradb-cluster-operator@sha256:<update_operator_SHA_value>" |
-        .metadata.name = strenv(name_certified)'\
-        "${bundle_directory}/manifests/${file_name}.clusterserviceversion.yaml"
+        .metadata.name = strenv(name_certified)' \
+		"${bundle_directory}/manifests/${file_name}.clusterserviceversion.yaml"
 
-elif [[ "${DISTRIBUTION}" == "marketplace" ]];then
-    # Annotations needed when targeting Red Hat Marketplace
-export package_url="https://marketplace.redhat.com/en-us/operators/${file_name}"
-    yq --inplace '
+elif [[ ${DISTRIBUTION} == "marketplace" ]]; then
+	# Annotations needed when targeting Red Hat Marketplace
+	export package_url="https://marketplace.redhat.com/en-us/operators/${file_name}"
+	yq --inplace '
         .metadata.name = env(name_certified_rhmp) |
         .metadata.annotations["containerImage"] = "registry.connect.redhat.com/percona/percona-xtradb-cluster-operator@sha256:<update_operator_SHA_value>" |
         .metadata.annotations["marketplace.openshift.io/remote-workflow"] =
@@ -169,7 +172,7 @@ export package_url="https://marketplace.redhat.com/en-us/operators/${file_name}"
         .metadata.annotations["marketplace.openshift.io/support-workflow"] =
             "https://marketplace.redhat.com/en-us/operators/percona-xtradb-cluster-operator-certified-rhmp/support?utm_source=openshift_console" |
         .spec.relatedImages = env(relatedImages)' \
-        "${bundle_directory}/manifests/${file_name}.clusterserviceversion.yaml"
+		"${bundle_directory}/manifests/${file_name}.clusterserviceversion.yaml"
 fi
 
-if > /dev/null command -v tree; then tree -C "${bundle_directory}"; fi
+if >/dev/null command -v tree; then tree -C "${bundle_directory}"; fi

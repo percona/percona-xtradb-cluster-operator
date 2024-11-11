afe047de45ebd25dee962cccfa3c848455fe1222
Ege Güneş <ege.gunes@percona.com>
2024-11-11 14:03:22 +0300

    K8SPXC-1222: Implement NOTIFY_SOCKET and state-monitor (#1836)
    * K8SPXC-1222: Implement NOTIFY_SOCKET and state-monitor
    
    It's very hard to understand if MySQL is starting up since you can't
    connect to server and query its state during startup. For this reason we
    needed a better way to query MySQL state.
    
    MySQL team informed us about `NOTIFY_SOCKET` env variable. If you set
    it, MySQL will send its state to this socket whenever state changes. We
    created a small utility (`state-monitor`) to read from this socket,
    parse the data and map MySQL status to one of the following states:
    
    1. unknown
    2. down
    3. startup
    4. ready
    
    state-monitor will write the state to file specified with
    `MYSQL_STATE_FILE`. By default it's `/var/lib/mysql/mysql.state`. Then
    we can use state file's contents wherever necessary. For example, with
    these changes, we use it to:
    
    1. Wait until MySQL initialization finishes in pxc-entrypoint.sh
    2. Check if MySQL state is `startup` in liveness probe and don't kill
       the PXC container if it's starting up
    3. Mark PXC container ready only if state file says it's ready too
    
    * fix tests
    
    * fix ineffassign
    
    * handle shutdown properly
    
    * fix addr already in use
    
    * fix golangci-lint
    
    * change executable name
    
    * fix status checking during upgrade
    
    * improve sysbench
    
    * bump k8s version
    
    * don't use state monitor in 5.7
    
    * address review comments
    
    * address review comments
    
    * add mysql version check to liveness
    
    ---------
    
    Co-authored-by: Viacheslav Sarzhan <slava.sarzhan@percona.com>

---
+++ cmd/mysql-state-monitor/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+)
+
+type MySQLState string
+
+const (
+	MySQLReady   MySQLState = "ready"
+	MySQLDown    MySQLState = "down"
+	MySQLStartup MySQLState = "startup"
+	MySQLUnknown MySQLState = "unknown"
+)
+
+func parseDatum(datum string) MySQLState {
+	lines := strings.Split(datum, "\n")
+
+	if lines[0] == "READY=1" {
+		return MySQLReady
+	}
+
+	if lines[0] == "STOPPING=1" {
+		return MySQLDown
+	}
+
+	if strings.HasPrefix(lines[0], "STATUS=") {
+		status := strings.TrimPrefix(lines[0], "STATUS=")
+		switch status {
+		case "Server is operational":
+			return MySQLReady
+		case "Server shutdown in progress":
+			return MySQLDown
+		case "Server startup in progress",
+			"Data Dictionary upgrade in progress",
+			"Data Dictionary upgrade complete",
+			"Server upgrade in progress",
+			"Server upgrade complete",
+			"Server downgrade in progress",
+			"Server downgrade complete",
+			"Data Dictionary upgrade from MySQL 5.7 in progress",
+			"Data Dictionary upgrade from MySQL 5.7 complete",
+			"Server shutdown complete": // we treat this as startup because during init, MySQL notifies this even if it's up
+			return MySQLStartup
+		}
+	}
+
+	return MySQLUnknown
+}
+
+func main() {
+	log.Println("Starting mysql-state-monitor")
+
+	socketPath, ok := os.LookupEnv("NOTIFY_SOCKET")
+	if !ok {
+		log.Fatalln("NOTIFY_SOCKET env variable is required")
+	}
+
+	stateFilePath, ok := os.LookupEnv("MYSQL_STATE_FILE")
+	if !ok {
+		log.Fatalln("MYSQL_STATE_FILE env variable is required")
+	}
+
+	stateFile, err := os.Create(stateFilePath)
+	if err != nil {
+		log.Fatalf("Failed to create state file: %s", err)
+	}
+	defer stateFile.Close()
+
+	if _, err := os.Stat(socketPath); err == nil {
+		if err := os.Remove(socketPath); err != nil {
+			log.Fatalf("Failed to remove %s: %s", socketPath, err)
+		}
+	}
+
+	addr, err := net.ResolveUnixAddr("unixgram", socketPath)
+	if err != nil {
+		log.Fatalf("Failed to resolve unix addr %s: %s", socketPath, err)
+	}
+
+	conn, err := net.ListenUnixgram("unixgram", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen unixgram %s: %s", socketPath, err)
+	}
+	defer conn.Close()
+
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, os.Interrupt, os.Kill)
+
+	go func() {
+		sig := <-sigterm
+		log.Printf("Received signal %v. Exiting mysql-state-monitor", sig)
+		os.Exit(0)
+	}()
+
+	buf := make([]byte, 256)
+	for {
+		n, _, err := conn.ReadFromUnix(buf)
+		if err != nil {
+			log.Printf("Failed to read from unix socket: %s", err)
+			continue
+		}
+		datum := string(buf[:n])
+		mysqlState := parseDatum(datum)
+
+		log.Printf("MySQLState: %s\nReceived: %s", mysqlState, datum)
+
+		err = stateFile.Truncate(0)
+		if err != nil {
+			log.Printf("Failed to truncate state file: %s", err)
+			continue
+		}
+
+		_, err = stateFile.Write([]byte(mysqlState))
+		if err != nil {
+			log.Printf("Failed to write to state file: %s", err)
+		}
+	}
+}

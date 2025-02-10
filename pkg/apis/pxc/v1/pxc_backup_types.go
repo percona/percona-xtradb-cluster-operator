percona/percona-xtradb-cluster-operator

commit 5a07740910eb4086f919eb6d9210daccfcc31866

K8SPXC-1366: Add startingDeadlineSeconds
We introduce a new field to PerconaXtraDBClusterBackup: `spec.startingDeadlineSeconds`.
If this field is set and if backup is not started in configured
duration, operator will automatically fail the backup. This duration is
checked by comparing the current time with backup job's creation
timestamp.

config/crd/bases/pxc.percona.com_perconaxtradbclusterbackups.yaml
@@ -146,6 +146,9 @@ spec:
                 type: object
               pxcCluster:
                 type: string
+              startingDeadlineSeconds:
+                format: int64
+                type: integer
               storageName:
                 type: string
             type: object
@@ -202,6 +205,8 @@ spec:
                   type: object
                 type: array
               destination:
+                type: string
+              error:
                 type: string
               image:
                 type: string

config/crd/bases/pxc.percona.com_perconaxtradbclusters.yaml
@@ -143,6 +143,9 @@ spec:
                     type: array
                   serviceAccountName:
                     type: string
+                  startingDeadlineSeconds:
+                    format: int64
+                    type: integer
                   storages:
                     additionalProperties:
                       properties:

deploy/backup/backup.yaml
@@ -8,6 +8,7 @@ spec:
   pxcCluster: cluster1
   storageName: fs-pvc
 #  activeDeadlineSeconds: 3600
+#  startingDeadlineSeconds: 300
 #  containerOptions:
 #    env:
 #    - name: VERIFY_TLS

deploy/bundle.yaml
@@ -145,6 +145,9 @@ spec:
                 type: object
               pxcCluster:
                 type: string
+              startingDeadlineSeconds:
+                format: int64
+                type: integer
               storageName:
                 type: string
             type: object
@@ -201,6 +204,8 @@ spec:
                   type: object
                 type: array
               destination:
+                type: string
+              error:
                 type: string
               image:
                 type: string
@@ -343,6 +348,8 @@ spec:
                       type: object
                     type: array
                   destination:
+                    type: string
+                  error:
                     type: string
                   image:
                     type: string
@@ -517,6 +524,8 @@ spec:
                           type: object
                         type: array
                       destination:
+                        type: string
+                      error:
                         type: string
                       image:
                         type: string
@@ -1048,6 +1057,9 @@ spec:
                     type: array
                   serviceAccountName:
                     type: string
+                  startingDeadlineSeconds:
+                    format: int64
+                    type: integer
                   storages:
                     additionalProperties:
                       properties:

deploy/cr.yaml
@@ -605,6 +605,7 @@ spec:
     image: perconalab/percona-xtradb-cluster-operator:main-pxc8.0-backup
 #    backoffLimit: 6
 #    activeDeadlineSeconds: 3600
+#    startingDeadlineSeconds: 300
 #    serviceAccountName: percona-xtradb-cluster-operator
 #    imagePullSecrets:
 #      - name: private-registry-credentials

deploy/crd.yaml
@@ -145,6 +145,9 @@ spec:
                 type: object
               pxcCluster:
                 type: string
+              startingDeadlineSeconds:
+                format: int64
+                type: integer
               storageName:
                 type: string
             type: object
@@ -201,6 +204,8 @@ spec:
                   type: object
                 type: array
               destination:
+                type: string
+              error:
                 type: string
               image:
                 type: string
@@ -343,6 +348,8 @@ spec:
                       type: object
                     type: array
                   destination:
+                    type: string
+                  error:
                     type: string
                   image:
                     type: string
@@ -517,6 +524,8 @@ spec:
                           type: object
                         type: array
                       destination:
+                        type: string
+                      error:
                         type: string
                       image:
                         type: string
@@ -1048,6 +1057,9 @@ spec:
                     type: array
                   serviceAccountName:
                     type: string
+                  startingDeadlineSeconds:
+                    format: int64
+                    type: integer
                   storages:
                     additionalProperties:
                       properties:

deploy/cw-bundle.yaml
@@ -145,6 +145,9 @@ spec:
                 type: object
               pxcCluster:
                 type: string
+              startingDeadlineSeconds:
+                format: int64
+                type: integer
               storageName:
                 type: string
             type: object
@@ -201,6 +204,8 @@ spec:
                   type: object
                 type: array
               destination:
+                type: string
+              error:
                 type: string
               image:
                 type: string
@@ -343,6 +348,8 @@ spec:
                       type: object
                     type: array
                   destination:
+                    type: string
+                  error:
                     type: string
                   image:
                     type: string
@@ -517,6 +524,8 @@ spec:
                           type: object
                         type: array
                       destination:
+                        type: string
+                      error:
                         type: string
                       image:
                         type: string
@@ -1048,6 +1057,9 @@ spec:
                     type: array
                   serviceAccountName:
                     type: string
+                  startingDeadlineSeconds:
+                    format: int64
+                    type: integer
                   storages:
                     additionalProperties:
                       properties:

pkg/apis/pxc/v1/pxc_backup_types.go
@@ -47,14 +47,16 @@ type PerconaXtraDBClusterBackup struct {
 }
 
 type PXCBackupSpec struct {
-	PXCCluster            string                  `json:"pxcCluster"`
-	StorageName           string                  `json:"storageName,omitempty"`
-	ContainerOptions      *BackupContainerOptions `json:"containerOptions,omitempty"`
-	ActiveDeadlineSeconds *int64                  `json:"activeDeadlineSeconds,omitempty"`
+	PXCCluster              string                  `json:"pxcCluster"`
+	StorageName             string                  `json:"storageName,omitempty"`
+	ContainerOptions        *BackupContainerOptions `json:"containerOptions,omitempty"`
+	StartingDeadlineSeconds *int64                  `json:"startingDeadlineSeconds,omitempty"`
+	ActiveDeadlineSeconds   *int64                  `json:"activeDeadlineSeconds,omitempty"`
 }
 
 type PXCBackupStatus struct {
 	State                 PXCBackupState          `json:"state,omitempty"`
+	Error                 string                  `json:"error,omitempty"`
 	CompletedAt           *metav1.Time            `json:"completed,omitempty"`
 	LastScheduled         *metav1.Time            `json:"lastscheduled,omitempty"`
 	Destination           PXCBackupDestination    `json:"destination,omitempty"`
@@ -185,4 +187,9 @@ func (cr *PerconaXtraDBClusterBackup) OwnerRef(scheme *runtime.Scheme) (metav1.O
 		UID:        cr.GetUID(),
 		Controller: &trueVar,
 	}, nil
+}
+
+func (cr *PerconaXtraDBClusterBackup) SetFailedStatusWithError(err error) {
+	cr.Status.State = BackupFailed
+	cr.Status.Error = err.Error()
 }

commit 025ae1ad8eed4b1497214c4e65a1eff61aeb3315

K8SPXC-1366: Add suspendedDeadlineSeconds
suspendedDeadlineSeconds allows user to configure maximum duration that
backup job can wait in suspended state. This field is optional and can
be configured in two separate places:

1. PerconaXtraDBClusterBackup.spec.suspendedDeadlineSeconds: Always used if defined.
2. PerconaXtraDBCluster.spec.backup.suspendedDeadlineSeconds: Used only if not defined in PerconaXtraDBClusterBackup.

Suspended deadlines seconds are checked against the last transition time
of job status condidition with type JobSuspended and status true.

Commit 5a077409 introduced startingDeadlineSeconds to fail backup it
doesn't start before configured deadline. This commit also allows user
to configure it globally, just like suspendedDeadlineSeconds.

startingDeadlineSeconds is an optinal field and can be configured in two
separate places:

1. PerconaXtraDBClusterBackup.spec.startedDeadlineSeconds: Always used if defined.
2. PerconaXtraDBCluster.spec.backup.startedDeadlineSeconds: Used only if not defined in PerconaXtraDBClusterBackup.

config/crd/bases/pxc.percona.com_perconaxtradbclusterbackups.yaml
@@ -151,6 +151,9 @@ spec:
                 type: integer
               storageName:
                 type: string
+              suspendedDeadlineSeconds:
+                format: int64
+                type: integer
             type: object
           status:
             properties:

config/crd/bases/pxc.percona.com_perconaxtradbclusters.yaml
@@ -1083,6 +1083,9 @@ spec:
                           type: object
                       type: object
                     type: object
+                  suspendedDeadlineSeconds:
+                    format: int64
+                    type: integer
                 type: object
               crVersion:
                 type: string

deploy/backup/backup.yaml
@@ -9,6 +9,7 @@ spec:
   storageName: fs-pvc
 #  activeDeadlineSeconds: 3600
 #  startingDeadlineSeconds: 300
+#  suspendedDeadlineSeconds: 1200
 #  containerOptions:
 #    env:
 #    - name: VERIFY_TLS

deploy/bundle.yaml
@@ -150,6 +150,9 @@ spec:
                 type: integer
               storageName:
                 type: string
+              suspendedDeadlineSeconds:
+                format: int64
+                type: integer
             type: object
           status:
             properties:
@@ -1997,6 +2000,9 @@ spec:
                           type: object
                       type: object
                     type: object
+                  suspendedDeadlineSeconds:
+                    format: int64
+                    type: integer
                 type: object
               crVersion:
                 type: string

deploy/cr.yaml
@@ -606,6 +606,7 @@ spec:
 #    backoffLimit: 6
 #    activeDeadlineSeconds: 3600
 #    startingDeadlineSeconds: 300
+#    suspendedDeadlineSeconds: 1200
 #    serviceAccountName: percona-xtradb-cluster-operator
 #    imagePullSecrets:
 #      - name: private-registry-credentials

deploy/crd.yaml
@@ -150,6 +150,9 @@ spec:
                 type: integer
               storageName:
                 type: string
+              suspendedDeadlineSeconds:
+                format: int64
+                type: integer
             type: object
           status:
             properties:
@@ -1997,6 +2000,9 @@ spec:
                           type: object
                       type: object
                     type: object
+                  suspendedDeadlineSeconds:
+                    format: int64
+                    type: integer
                 type: object
               crVersion:
                 type: string

deploy/cw-bundle.yaml
@@ -150,6 +150,9 @@ spec:
                 type: integer
               storageName:
                 type: string
+              suspendedDeadlineSeconds:
+                format: int64
+                type: integer
             type: object
           status:
             properties:
@@ -1997,6 +2000,9 @@ spec:
                           type: object
                       type: object
                     type: object
+                  suspendedDeadlineSeconds:
+                    format: int64
+                    type: integer
                 type: object
               crVersion:
                 type: string

pkg/apis/pxc/v1/pxc_backup_types.go
@@ -47,11 +47,12 @@ type PerconaXtraDBClusterBackup struct {
 }
 
 type PXCBackupSpec struct {
-	PXCCluster              string                  `json:"pxcCluster"`
-	StorageName             string                  `json:"storageName,omitempty"`
-	ContainerOptions        *BackupContainerOptions `json:"containerOptions,omitempty"`
-	StartingDeadlineSeconds *int64                  `json:"startingDeadlineSeconds,omitempty"`
-	ActiveDeadlineSeconds   *int64                  `json:"activeDeadlineSeconds,omitempty"`
+	PXCCluster               string                  `json:"pxcCluster"`
+	StorageName              string                  `json:"storageName,omitempty"`
+	ContainerOptions         *BackupContainerOptions `json:"containerOptions,omitempty"`
+	ActiveDeadlineSeconds    *int64                  `json:"activeDeadlineSeconds,omitempty"`
+	StartingDeadlineSeconds  *int64                  `json:"startingDeadlineSeconds,omitempty"`
+	SuspendedDeadlineSeconds *int64                  `json:"suspendedDeadlineSeconds,omitempty"`
 }
 
 type PXCBackupStatus struct {

pkg/apis/pxc/v1/pxc_types.go
@@ -160,18 +160,19 @@ const (
 )
 
 type PXCScheduledBackup struct {
-	AllowParallel           *bool                         `json:"allowParallel,omitempty"`
-	Image                   string                        `json:"image,omitempty"`
-	ImagePullSecrets        []corev1.LocalObjectReference `json:"imagePullSecrets,omitempty"`
-	ImagePullPolicy         corev1.PullPolicy             `json:"imagePullPolicy,omitempty"`
-	Schedule                []PXCScheduledBackupSchedule  `json:"schedule,omitempty"`
-	Storages                map[string]*BackupStorageSpec `json:"storages,omitempty"`
-	ServiceAccountName      string                        `json:"serviceAccountName,omitempty"`
-	Annotations             map[string]string             `json:"annotations,omitempty"`
-	PITR                    PITRSpec                      `json:"pitr,omitempty"`
-	BackoffLimit            *int32                        `json:"backoffLimit,omitempty"`
-	ActiveDeadlineSeconds   *int64                        `json:"activeDeadlineSeconds,omitempty"`
-	StartingDeadlineSeconds *int64                        `json:"startingDeadlineSeconds,omitempty"`
+	AllowParallel            *bool                         `json:"allowParallel,omitempty"`
+	Image                    string                        `json:"image,omitempty"`
+	ImagePullSecrets         []corev1.LocalObjectReference `json:"imagePullSecrets,omitempty"`
+	ImagePullPolicy          corev1.PullPolicy             `json:"imagePullPolicy,omitempty"`
+	Schedule                 []PXCScheduledBackupSchedule  `json:"schedule,omitempty"`
+	Storages                 map[string]*BackupStorageSpec `json:"storages,omitempty"`
+	ServiceAccountName       string                        `json:"serviceAccountName,omitempty"`
+	Annotations              map[string]string             `json:"annotations,omitempty"`
+	PITR                     PITRSpec                      `json:"pitr,omitempty"`
+	BackoffLimit             *int32                        `json:"backoffLimit,omitempty"`
+	ActiveDeadlineSeconds    *int64                        `json:"activeDeadlineSeconds,omitempty"`
+	StartingDeadlineSeconds  *int64                        `json:"startingDeadlineSeconds,omitempty"`
+	SuspendedDeadlineSeconds *int64                        `json:"suspendedDeadlineSeconds,omitempty"`
 }
 
 func (b *PXCScheduledBackup) GetAllowParallel() bool {

pkg/apis/pxc/v1/zz_generated.deepcopy.go
@@ -401,13 +401,18 @@ func (in *PXCBackupSpec) DeepCopyInto(out *PXCBackupSpec) {
 		*out = new(BackupContainerOptions)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.ActiveDeadlineSeconds != nil {
+		in, out := &in.ActiveDeadlineSeconds, &out.ActiveDeadlineSeconds
+		*out = new(int64)
+		**out = **in
+	}
 	if in.StartingDeadlineSeconds != nil {
 		in, out := &in.StartingDeadlineSeconds, &out.StartingDeadlineSeconds
 		*out = new(int64)
 		**out = **in
 	}
-	if in.ActiveDeadlineSeconds != nil {
-		in, out := &in.ActiveDeadlineSeconds, &out.ActiveDeadlineSeconds
+	if in.SuspendedDeadlineSeconds != nil {
+		in, out := &in.SuspendedDeadlineSeconds, &out.SuspendedDeadlineSeconds
 		*out = new(int64)
 		**out = **in
 	}
@@ -526,6 +531,11 @@ func (in *PXCScheduledBackup) DeepCopyInto(out *PXCScheduledBackup) {
 	}
 	if in.StartingDeadlineSeconds != nil {
 		in, out := &in.StartingDeadlineSeconds, &out.StartingDeadlineSeconds
+		*out = new(int64)
+		**out = **in
+	}
+	if in.SuspendedDeadlineSeconds != nil {
+		in, out := &in.SuspendedDeadlineSeconds, &out.SuspendedDeadlineSeconds
 		*out = new(int64)
 		**out = **in
 	}

pkg/controller/pxcbackup/controller.go
@@ -103,6 +103,11 @@ type ReconcilePerconaXtraDBClusterBackup struct {
 	bcpDeleteInProgress *sync.Map
 }
 
+var (
+	errSuspendedDeadlineExceeded = errors.New("suspended deadline seconds exceeded")
+	errStartingDeadlineExceeded  = errors.New("starting deadline seconds exceeded")
+)
+
 // Reconcile reads that state of the cluster for a PerconaXtraDBClusterBackup object and makes changes based on the state read
 // and what is in the PerconaXtraDBClusterBackup.Spec
 // Note:
@@ -151,14 +156,6 @@ func (r *ReconcilePerconaXtraDBClusterBackup) Reconcile(ctx context.Context, req
 		return rr, nil
 	}
 
-	if err := r.checkStartingDeadline(ctx, cr); err != nil {
-		if err := r.setFailedStatus(ctx, cr, err); err != nil {
-			return rr, errors.Wrap(err, "update status")
-		}
-
-		return reconcile.Result{}, nil
-	}
-
 	cluster, err := r.getCluster(ctx, cr)
 	if err != nil {
 		return reconcile.Result{}, errors.Wrap(err, "get cluster")
@@ -185,6 +182,21 @@ func (r *ReconcilePerconaXtraDBClusterBackup) Reconcile(ctx context.Context, req
 		}
 
 		return reconcile.Result{}, err
+	}
+
+	if err := r.checkDeadlines(ctx, cluster, cr); err != nil {
+		if err := r.setFailedStatus(ctx, cr, err); err != nil {
+			return rr, errors.Wrap(err, "update status")
+		}
+
+		if errors.Is(err, errSuspendedDeadlineExceeded) {
+			log.Info("cleaning up suspended backup job")
+			if err := r.cleanUpSuspendedJob(ctx, cluster, cr); err != nil {
+				return reconcile.Result{}, errors.Wrap(err, "clean up suspended job")
+			}
+		}
+
+		return reconcile.Result{}, nil
 	}
 
 	if err := r.reconcileBackupJob(ctx, cr, cluster); err != nil {
@@ -661,24 +673,86 @@ func (r *ReconcilePerconaXtraDBClusterBackup) updateJobStatus(
 	return nil
 }
 
-func (r *ReconcilePerconaXtraDBClusterBackup) checkStartingDeadline(ctx context.Context, cr *api.PerconaXtraDBClusterBackup) error {
+func (r *ReconcilePerconaXtraDBClusterBackup) checkDeadlines(ctx context.Context, cluster *api.PerconaXtraDBCluster, cr *api.PerconaXtraDBClusterBackup) error {
+	if err := checkStartingDeadline(ctx, cluster, cr); err != nil {
+		return err
+	}
+
+	if err := r.checkSuspendedDeadline(ctx, cluster, cr); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func checkStartingDeadline(ctx context.Context, cluster *api.PerconaXtraDBCluster, cr *api.PerconaXtraDBClusterBackup) error {
 	log := logf.FromContext(ctx)
 
+	if cr.Status.State != api.BackupNew {
+		return nil
+	}
+
+	var deadlineSeconds *int64
+	if cr.Spec.StartingDeadlineSeconds != nil {
+		deadlineSeconds = cr.Spec.StartingDeadlineSeconds
+	} else if cluster.Spec.Backup.StartingDeadlineSeconds != nil {
+		deadlineSeconds = cluster.Spec.Backup.StartingDeadlineSeconds
+	}
+
+	if deadlineSeconds == nil {
+		return nil
+	}
+
 	since := time.Since(cr.CreationTimestamp.Time).Seconds()
-
-	if cr.Spec.StartingDeadlineSeconds == nil {
+	if since < float64(*deadlineSeconds) {
 		return nil
 	}
 
-	if since < float64(*cr.Spec.StartingDeadlineSeconds) {
+	log.Info("Backup didn't start in startingDeadlineSeconds, failing the backup",
+		"startingDeadlineSeconds", *deadlineSeconds,
+		"passedSeconds", since)
+
+	return errStartingDeadlineExceeded
+}
+
+func (r *ReconcilePerconaXtraDBClusterBackup) checkSuspendedDeadline(
+	ctx context.Context,
+	cluster *api.PerconaXtraDBCluster,
+	cr *api.PerconaXtraDBClusterBackup,
+) error {
+	log := logf.FromContext(ctx)
+
+	job, err := r.getBackupJob(ctx, cluster, cr)
+	if err != nil {
+		if k8sErrors.IsNotFound(err) {
+			return nil
+		}
+
+		return err
+	}
+
+	var deadlineSeconds *int64
+	if cr.Spec.SuspendedDeadlineSeconds != nil {
+		deadlineSeconds = cr.Spec.SuspendedDeadlineSeconds
+	} else if cluster.Spec.Backup.SuspendedDeadlineSeconds != nil {
+		deadlineSeconds = cluster.Spec.Backup.SuspendedDeadlineSeconds
+	}
+
+	if deadlineSeconds == nil {
 		return nil
 	}
 
-	if cr.Status.State == api.BackupNew {
-		log.Info("Backup didn't start in startingDeadlineSeconds, failing the backup",
-			"startingDeadlineSeconds", *cr.Spec.StartingDeadlineSeconds,
-			"passedSeconds", since)
-		return errors.New("starting deadline seconds exceeded")
+	for _, cond := range job.Status.Conditions {
+		if cond.Type != batchv1.JobSuspended || cond.Status != corev1.ConditionTrue {
+			continue
+		}
+
+		if since := time.Since(cond.LastTransitionTime.Time).Seconds(); since > float64(*deadlineSeconds) {
+			log.Info("Backup didn't resume in suspendedDeadlineSeconds, failing the backup",
+				"suspendedDeadlineSeconds", *deadlineSeconds,
+				"passedSeconds", since)
+			return errSuspendedDeadlineExceeded
+		}
 	}
 
 	return nil
@@ -726,13 +800,8 @@ func (r *ReconcilePerconaXtraDBClusterBackup) suspendJobIfNeeded(
 
 	log := logf.FromContext(ctx)
 
-	labelKeyBackupType := naming.GetLabelBackupType(cluster)
-	jobName := naming.BackupJobName(cr.Name, cr.Labels[labelKeyBackupType] == "cron")
-
 	err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		job := new(batchv1.Job)
-
-		err := r.client.Get(ctx, types.NamespacedName{Namespace: cr.Namespace, Name: jobName}, job)
+		job, err := r.getBackupJob(ctx, cluster, cr)
 		if err != nil {
 			if k8sErrors.IsNotFound(err) {
 				return nil
@@ -752,7 +821,7 @@ func (r *ReconcilePerconaXtraDBClusterBackup) suspendJobIfNeeded(
 		}
 
 		log.Info("Suspending backup job",
-			"job", jobName,
+			"job", job.Name,
 			"clusterStatus", cluster.Status.Status,
 			"readyPXC", cluster.Status.PXC.Ready)
 
@@ -785,13 +854,8 @@ func (r *ReconcilePerconaXtraDBClusterBackup) resumeJobIfNeeded(
 
 	log := logf.FromContext(ctx)
 
-	labelKeyBackupType := naming.GetLabelBackupType(cluster)
-	jobName := naming.BackupJobName(cr.Name, cr.Labels[labelKeyBackupType] == "cron")
-
 	err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		job := new(batchv1.Job)
-
-		err := r.client.Get(ctx, types.NamespacedName{Namespace: cr.Namespace, Name: jobName}, job)
+		job, err := r.getBackupJob(ctx, cluster, cr)
 		if err != nil {
 			if k8sErrors.IsNotFound(err) {
 				return nil
@@ -811,7 +875,7 @@ func (r *ReconcilePerconaXtraDBClusterBackup) resumeJobIfNeeded(
 		}
 
 		log.Info("Resuming backup job",
-			"job", jobName,
+			"job", job.Name,
 			"clusterStatus", cluster.Status.Status,
 			"readyPXC", cluster.Status.PXC.Ready)
 
@@ -834,6 +898,41 @@ func (r *ReconcilePerconaXtraDBClusterBackup) reconcileBackupJob(
 
 	if err := r.resumeJobIfNeeded(ctx, cr, cluster); err != nil {
 		return errors.Wrap(err, "suspend job if needed")
+	}
+
+	return nil
+}
+
+func (r *ReconcilePerconaXtraDBClusterBackup) getBackupJob(
+	ctx context.Context,
+	cluster *api.PerconaXtraDBCluster,
+	cr *api.PerconaXtraDBClusterBackup,
+) (*batchv1.Job, error) {
+	labelKeyBackupType := naming.GetLabelBackupType(cluster)
+	jobName := naming.BackupJobName(cr.Name, cr.Labels[labelKeyBackupType] == "cron")
+
+	job := new(batchv1.Job)
+
+	err := r.client.Get(ctx, types.NamespacedName{Namespace: cr.Namespace, Name: jobName}, job)
+	if err != nil {
+		return nil, err
+	}
+
+	return job, nil
+}
+
+func (r *ReconcilePerconaXtraDBClusterBackup) cleanUpSuspendedJob(
+	ctx context.Context,
+	cluster *api.PerconaXtraDBCluster,
+	cr *api.PerconaXtraDBClusterBackup,
+) error {
+	job, err := r.getBackupJob(ctx, cluster, cr)
+	if err != nil {
+		return errors.Wrap(err, "get job")
+	}
+
+	if err := r.client.Delete(ctx, job); err != nil {
+		return errors.Wrap(err, "delete job")
 	}
 
 	return nil
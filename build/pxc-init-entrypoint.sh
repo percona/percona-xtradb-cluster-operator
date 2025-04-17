d313f6a3ebbc5ea946cde40697ec69a9e6d11971
Ege Güneş <ege.gunes@percona.com>
2025-04-17 11:06:34 +0300

    K8SPXC-1376: Don't require matching passwords for restoring a backup
    This commit adds a post-restore step to change passwords in restored
    backup so operator can continue reconciling cluster with existing
    secrets and liveness/readiness work.
    
    After backup is restored, operator will create a another job that starts
    `mysqld` with `--skip-grant-tables` and change user passwords in the
    database by using passwords in the secrets. If some system users are not
    in the database, this job will create them. If users don't have some
    grants, those will be added to users as well.

--- build/pxc-init-entrypoint.sh
+++ build/pxc-init-entrypoint.sh
@@ -13,3 +13,4 @@ install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /get-pxc-state /var/lib/mysql/get
 install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /pmm-prerun.sh /var/lib/mysql/pmm-prerun.sh
 install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /mysql-state-monitor /var/lib/mysql/mysql-state-monitor
 install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /wsrep_cmd_notify_handler.sh /var/lib/mysql/wsrep_cmd_notify_handler.sh
+install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /prepare_restored_cluster.sh /var/lib/mysql/prepare_restored_cluster.sh

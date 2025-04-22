0ba3378fbe96afce36a91df8632477719f4bcec2
George Kechagias <geo.kechagias@gmail.com>
2025-04-22 11:18:15 +0300

    add some more unit tests

--- pkg/pxc/service_test.go
+++ pkg/pxc/service_test.go
@@ -1,10 +1,10 @@
 package pxc
 
 import (
-	corev1 "k8s.io/api/core/v1"
 	"testing"
 
 	api "github.com/percona/percona-xtradb-cluster-operator/pkg/apis/pxc/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -58,6 +58,7 @@ func TestNewServiceProxySQL(t *testing.T) {
 
 func TestNewServiceHAProxy(t *testing.T) {
 	lbClass := "custom-lb-class"
+	lbClassTwo := "custom-lb-class-two"
 
 	tests := map[string]struct {
 		crVersion             string
@@ -65,11 +66,15 @@ func TestNewServiceHAProxy(t *testing.T) {
 		replicasServiceExpose api.ServiceExpose
 		expectedServiceSpec   corev1.ServiceSpec
 	}{
-		"version >= 1.18.0 - primary service expose configured": {
+		"version >= 1.18.0 - primary service expose configured - ensure that primary config is used": {
 			crVersion: "1.18.0",
 			primaryServiceExpose: api.ServiceExpose{
 				Type:              "LoadBalancer",
 				LoadBalancerClass: &lbClass,
+			},
+			replicasServiceExpose: api.ServiceExpose{
+				Type:              "LoadBalancer",
+				LoadBalancerClass: &lbClassTwo,
 			},
 			expectedServiceSpec: corev1.ServiceSpec{
 				Type:              "LoadBalancer",
@@ -122,6 +127,87 @@ func TestNewServiceHAProxy(t *testing.T) {
 			}
 
 			svc := NewServiceHAProxy(cr)
+			if svc.Spec.Type != tt.expectedServiceSpec.Type {
+				t.Fatalf("expected LoadBalancerClass to be %v, got %v", tt.expectedServiceSpec.Type, svc.Spec.Type)
+			}
+			if svc.Spec.LoadBalancerClass != tt.expectedServiceSpec.LoadBalancerClass {
+				t.Fatalf("expected LoadBalancerClass to be %v, got %v", *tt.expectedServiceSpec.LoadBalancerClass, svc.Spec.LoadBalancerClass)
+			}
+		})
+	}
+}
+
+func TestNewServiceHAProxyReplicas(t *testing.T) {
+	lbClass := "custom-lb-class"
+	lbClassTwo := "custom-lb-class-two"
+
+	tests := map[string]struct {
+		crVersion             string
+		primaryServiceExpose  api.ServiceExpose
+		replicasServiceExpose api.ServiceExpose
+		expectedServiceSpec   corev1.ServiceSpec
+	}{
+		"version >= 1.18.0 - replica service expose configured - ensure replica config is used": {
+			crVersion: "1.18.0",
+			primaryServiceExpose: api.ServiceExpose{
+				Type:              "LoadBalancer",
+				LoadBalancerClass: &lbClass,
+			},
+			replicasServiceExpose: api.ServiceExpose{
+				Type:              "LoadBalancer",
+				LoadBalancerClass: &lbClassTwo,
+			},
+			expectedServiceSpec: corev1.ServiceSpec{
+				Type:              "LoadBalancer",
+				LoadBalancerClass: &lbClassTwo,
+			},
+		},
+		"version < 1.18.0 - replica service without lb class": {
+			crVersion: "1.17.0",
+			replicasServiceExpose: api.ServiceExpose{
+				Type:              "LoadBalancer",
+				LoadBalancerClass: &lbClass,
+			},
+			expectedServiceSpec: corev1.ServiceSpec{
+				Type: "LoadBalancer",
+			},
+		},
+		"version >= 1.18.0 - replica service without lb class": {
+			crVersion: "1.18.0",
+			replicasServiceExpose: api.ServiceExpose{
+				Type: "LoadBalancer",
+			},
+			expectedServiceSpec: corev1.ServiceSpec{
+				Type: "LoadBalancer",
+			},
+		},
+		"version < 1.18.0 - no lb expose configuration": {
+			crVersion: "1.17.0",
+			expectedServiceSpec: corev1.ServiceSpec{
+				Type: "ClusterIP",
+			},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			cr := &api.PerconaXtraDBCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "my-cluster",
+					Namespace: "my-namespace",
+				},
+				Spec: api.PerconaXtraDBClusterSpec{
+					CRVersion: tt.crVersion,
+					HAProxy: &api.HAProxySpec{
+						ExposePrimary: tt.primaryServiceExpose,
+						ExposeReplicas: &api.ReplicasServiceExpose{
+							ServiceExpose: tt.replicasServiceExpose,
+						},
+					},
+				},
+			}
+
+			svc := NewServiceHAProxyReplicas(cr)
 			if svc.Spec.Type != tt.expectedServiceSpec.Type {
 				t.Fatalf("expected LoadBalancerClass to be %v, got %v", tt.expectedServiceSpec.Type, svc.Spec.Type)
 			}

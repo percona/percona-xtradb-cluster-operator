83acf214dd72b34c10a92707f22b2ece270ac406
George Kechagias <geo.kechagias@gmail.com>
2025-04-24 14:28:03 +0300

    K8SPXC-1553 pmm3 support for pxc

--- pkg/apis/pxc/v1/pxc_types.go
+++ pkg/apis/pxc/v1/pxc_types.go
@@ -638,9 +638,10 @@ type LogCollectorSpec struct {
 }
 
 type PMMSpec struct {
-	Enabled                  bool                        `json:"enabled,omitempty"`
-	ServerHost               string                      `json:"serverHost,omitempty"`
-	Image                    string                      `json:"image,omitempty"`
+	Enabled    bool   `json:"enabled,omitempty"`
+	ServerHost string `json:"serverHost,omitempty"`
+	Image      string `json:"image,omitempty"`
+	// Deprecated, ServerUser is used for PMM2. PMM2 is reaching its EOL.
 	ServerUser               string                      `json:"serverUser,omitempty"`
 	CustomClusterName        string                      `json:"customClusterName,omitempty"`
 	PxcParams                string                      `json:"pxcParams,omitempty"`
@@ -657,6 +658,7 @@ func (spec *PMMSpec) IsEnabled(secret *corev1.Secret) bool {
 	return spec.Enabled && spec.HasSecret(secret)
 }
 
+// HasSecret is used for supporting PMM2.
 func (spec *PMMSpec) HasSecret(secret *corev1.Secret) bool {
 	for _, key := range []string{users.PMMServer, users.PMMServerKey} {
 		if _, ok := secret.Data[key]; ok {
@@ -666,6 +668,7 @@ func (spec *PMMSpec) HasSecret(secret *corev1.Secret) bool {
 	return false
 }
 
+// UseAPI is used for supporting PMM2.
 func (spec *PMMSpec) UseAPI(secret *corev1.Secret) bool {
 	if _, ok := secret.Data[users.PMMServerKey]; !ok {
 		if _, ok := secret.Data[users.PMMServer]; ok {

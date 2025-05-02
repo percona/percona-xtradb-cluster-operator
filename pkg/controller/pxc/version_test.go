499b2ba688acec53a408721e3557d4d160bf7ac3
Natalia Marukovich <natalia.marukovich@percona.com>
2025-05-02 12:22:32 +0200

    fix imports

--- pkg/controller/pxc/version_test.go
+++ pkg/controller/pxc/version_test.go
@@ -3,7 +3,7 @@ package pxc
 import (
 	"github.com/percona/percona-xtradb-cluster-operator/pkg/apis"
 	"github.com/percona/percona-xtradb-cluster-operator/pkg/k8s"
-	"github.com/percona/percona-xtradb-cluster-operator/version"
+	"github.com/percona/percona-xtradb-cluster-operator/pkg/version"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"

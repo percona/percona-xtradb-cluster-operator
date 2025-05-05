eeb9cbd8ccaa034ad68973e679bfa6b5d323e3c6
Ege Güneş <ege.gunes@percona.com>
2025-05-05 11:32:11 +0300

    K8SPXC-1376: Don't require matching passwords for restoring a backup (#2046)
    * K8SPXC-1376: Don't require matching passwords for restoring a backup
    
    This commit adds a post-restore step to change passwords in restored
    backup so operator can continue reconciling cluster with existing
    secrets and liveness/readiness work.
    
    After backup is restored, operator will create a another job that starts
    `mysqld` with `--skip-grant-tables` and change user passwords in the
    database by using passwords in the secrets. If some system users are not
    in the database, this job will create them. If users don't have some
    grants, those will be added to users as well.
    
    * fix prepare for encrypted clusters
    
    * fix password escaping in test
    
    * fix 5.7
    
    * remove xtrace
    
    * add unit test
    
    * fix unit tests
    
    * fix unit tests
    
    * fix tests
    
    ---------
    
    Co-authored-by: Viacheslav Sarzhan <slava.sarzhan@percona.com>

---
+++ pkg/pxc/backup/restore_test.go
@@ -0,0 +1,172 @@
+package backup
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sversion "k8s.io/apimachinery/pkg/version"
+	"k8s.io/utils/ptr"
+
+	pxcv1 "github.com/percona/percona-xtradb-cluster-operator/pkg/apis/pxc/v1"
+	"github.com/percona/percona-xtradb-cluster-operator/pkg/test"
+	"github.com/percona/percona-xtradb-cluster-operator/pkg/version"
+)
+
+func TestPrepareJob(t *testing.T) {
+	cluster := pxcv1.PerconaXtraDBCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-cluster",
+			Namespace: "test-ns",
+		},
+		Spec: pxcv1.PerconaXtraDBClusterSpec{
+			CRVersion: version.Version(),
+			Backup: &pxcv1.PXCScheduledBackup{
+				Image: "percona/percona-xtrabackup:8.0",
+				Storages: map[string]*pxcv1.BackupStorageSpec{
+					"test-storage": {
+						Type: pxcv1.BackupStorageS3,
+						S3: &pxcv1.BackupStorageS3Spec{
+							Bucket:            "operator-testing",
+							Region:            "us-west-1",
+							CredentialsSecret: "test-secret",
+						},
+					},
+				},
+			},
+			PXC: &pxcv1.PXCSpec{
+				PodSpec: &pxcv1.PodSpec{
+					Size:     3,
+					Image:    "percona/percona-xtradb-cluster:8.0",
+					Affinity: &pxcv1.PodAffinity{},
+					VolumeSpec: &pxcv1.VolumeSpec{
+						PersistentVolumeClaim: &corev1.PersistentVolumeClaimSpec{
+							Resources: corev1.VolumeResourceRequirements{
+								Requests: corev1.ResourceList{
+									corev1.ResourceStorage: resource.MustParse("1Gi"),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	sv := &version.ServerVersion{
+		Platform: version.PlatformKubernetes,
+		Info:     k8sversion.Info{},
+	}
+
+	err := cluster.CheckNSetDefaults(sv, log)
+	assert.NoError(t, err)
+
+	backup := pxcv1.PerconaXtraDBClusterBackup{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-backup",
+			Namespace: "test-ns",
+		},
+		Spec: pxcv1.PXCBackupSpec{
+			PXCCluster:  "test-cluster",
+			StorageName: "test-storage",
+		},
+		Status: pxcv1.PXCBackupStatus{
+			StorageName: "test-storage",
+		},
+	}
+
+	restore := pxcv1.PerconaXtraDBClusterRestore{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-restore",
+			Namespace: "test-ns",
+		},
+		Spec: pxcv1.PerconaXtraDBClusterRestoreSpec{
+			PXCCluster: "test-cluster",
+			BackupName: "test-backup",
+		},
+	}
+
+	initImage := "perconalab/percona-xtradb-cluster-operator:main"
+
+	cl := test.BuildFakeClient()
+
+	job, err := PrepareJob(&restore, &backup, &cluster, initImage, cl.Scheme())
+	assert.NoError(t, err)
+
+	assert.Equal(t, job.Spec.Template.Spec.Containers[0].Image, "percona/percona-xtradb-cluster:8.0")
+	assert.Equal(t, job.Spec.Template.Spec.Containers[0].Command, []string{"/var/lib/mysql/prepare_restored_cluster.sh"})
+
+	expectedVolumes := []corev1.Volume{
+		{
+			Name: "datadir",
+			VolumeSource: corev1.VolumeSource{
+				PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+					ClaimName: "datadir-test-cluster-pxc-0",
+				},
+			},
+		},
+		{
+			Name: "mysql-users-secret-file",
+			VolumeSource: corev1.VolumeSource{
+				Secret: &corev1.SecretVolumeSource{
+					SecretName: "internal-test-cluster",
+					Optional:   ptr.To(false),
+				},
+			},
+		},
+		{
+			Name: "vault-keyring-secret",
+			VolumeSource: corev1.VolumeSource{
+				Secret: &corev1.SecretVolumeSource{
+					SecretName: "test-cluster-vault",
+					Optional:   ptr.To(true),
+				},
+			},
+		},
+		{
+			Name: "ssl",
+			VolumeSource: corev1.VolumeSource{
+				Secret: &corev1.SecretVolumeSource{
+					SecretName: "test-cluster-ssl",
+					Optional:   ptr.To(false),
+				},
+			},
+		},
+		{
+			Name: "ssl-internal",
+			VolumeSource: corev1.VolumeSource{
+				Secret: &corev1.SecretVolumeSource{
+					SecretName: "test-cluster-ssl-internal",
+					Optional:   ptr.To(false),
+				},
+			},
+		},
+	}
+	assert.ElementsMatch(t, job.Spec.Template.Spec.Volumes, expectedVolumes)
+
+	expectedVolumeMounts := []corev1.VolumeMount{
+		{
+			Name:      "datadir",
+			MountPath: "/var/lib/mysql",
+		},
+		{
+			Name:      "mysql-users-secret-file",
+			MountPath: "/etc/mysql/mysql-users-secret",
+		},
+		{
+			Name:      "vault-keyring-secret",
+			MountPath: "/etc/mysql/vault-keyring-secret",
+		},
+		{
+			Name:      "ssl",
+			MountPath: "/etc/mysql/ssl",
+		},
+		{
+			Name:      "ssl-internal",
+			MountPath: "/etc/mysql/ssl-internal",
+		},
+	}
+	assert.ElementsMatch(t, job.Spec.Template.Spec.Containers[0].VolumeMounts, expectedVolumeMounts)
+}

f353014b8bda5e4eeb98e6e8fcc83c83c38c7f6c
Andrii Dema <a.dema@jazzserve.com>
2025-05-05 14:25:34 +0300

    delete comments

--- build/backup-init-entrypoint.sh
+++ build/backup-init-entrypoint.sh
@@ -8,6 +8,3 @@ install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /peer-list /opt/percona/peer-list
 mkdir -p /opt/percona/backup/lib/pxc
 install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /backup/lib/pxc/* /opt/percona/backup/lib/pxc/
 install -o "$(id -u)" -g "$(id -g)" -m 0755 -D /backup/recovery-*.sh backup/run_backup.sh backup/backup.sh /opt/percona/backup/
-
-#COPY lib/pxc /usr/lib/pxc
-#COPY recovery-*.sh run_backup.sh backup.sh get-pxc-state /usr/bin/

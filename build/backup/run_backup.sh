738ba1751449a11b997b6088a263a9de263254fb
Ege Güneş <ege.gunes@percona.com>
2025-05-15 19:18:40 +0300

    run 2nd socat in background so garbd can terminate recv script

--- build/backup/run_backup.sh
+++ build/backup/run_backup.sh
@@ -134,7 +134,8 @@ backup_s3() {
 		# shellcheck disable=SC2086
 		socat -u "$SOCAT_OPTS" stdio \
 			| xbcloud put --storage=s3 --parallel="$(grep -c processor /proc/cpuinfo)" --md5 $XBCLOUD_ARGS --s3-bucket="$S3_BUCKET" "$S3_BUCKET_PATH" 2>&1 \
-			| (grep -v "error: http request failed: Couldn't resolve host name" || exit 1)
+			| (grep -v "error: http request failed: Couldn't resolve host name" || exit 1) &
+		wait $!
 	fi
 
 	# shellcheck disable=SC2086

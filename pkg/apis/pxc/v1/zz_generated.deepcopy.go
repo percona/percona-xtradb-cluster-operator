ffbc079ad478020272c425dbc21c9510960c21ba
George Kechagias <geo.kechagias@gmail.com>
2025-11-03 13:56:58 +0200

    fix versions

--- pkg/apis/pxc/v1/zz_generated.deepcopy.go
+++ pkg/apis/pxc/v1/zz_generated.deepcopy.go
@@ -1368,7 +1368,7 @@ func (in *TLSSpec) DeepCopyInto(out *TLSSpec) {
 	}
 	if in.IssuerConf != nil {
 		in, out := &in.IssuerConf, &out.IssuerConf
-		*out = new(apismetav1.IssuerReference)
+		*out = new(apismetav1.ObjectReference)
 		**out = **in
 	}
 }

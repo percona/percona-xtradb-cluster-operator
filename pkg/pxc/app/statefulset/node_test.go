3e65dbaa920e3b8e557d9bfe6fe74bac0e02d49d
George Kechagias <geo.kechagias@gmail.com>
2025-11-26 18:33:15 +0200

    K8SPXC-1470 Implement migration between HAproxy and ProxySQL (#2253)
    * K8SPXC-1470 Implement migration between HAproxy and ProxySQL
    
    * fix test and error regatding hostgroups
    
    * remove unused param
    
    * update e2e test
    
    * add e2e test
    
    * update when the new test is running
    
    * fix upgrade-consistency test
    
    * improve proxy switch test
    
    * remove block for checking hostgroups
    
    * fix proxysql-scheduler test
    
    * enable scheduler on proxysql for proxy-switch test
    
    * fixes on the node test
    
    * import ordering
    
    ---------
    
    Co-authored-by: Viacheslav Sarzhan <slava.sarzhan@percona.com>

--- pkg/pxc/app/statefulset/node_test.go
+++ pkg/pxc/app/statefulset/node_test.go
@@ -3,109 +3,122 @@ package statefulset
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/utils/ptr"
+	"k8s.io/apimachinery/pkg/types"
 
 	api "github.com/percona/percona-xtradb-cluster-operator/pkg/apis/pxc/v1"
+	"github.com/percona/percona-xtradb-cluster-operator/pkg/pxc/app"
+	"github.com/percona/percona-xtradb-cluster-operator/pkg/pxc/users"
 	"github.com/percona/percona-xtradb-cluster-operator/pkg/test"
 	"github.com/percona/percona-xtradb-cluster-operator/pkg/version"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-func TestPXCAppContainer(t *testing.T) {
+func TestAppContainer(t *testing.T) {
+
+	secretName := "my-secret"
+
 	tests := map[string]struct {
-		spec              api.PXCSpec
-		secrets           string
+		spec              api.PerconaXtraDBClusterSpec
+		crUID             types.UID
+		expectedContainer func() corev1.Container
 		envFromSecret     *corev1.Secret
-		expectedName      string
-		expectedImage     string
-		expectedLDPreload string
-		expectedEnvFrom   []corev1.EnvFromSource
 	}{
-		"success - container construction": {
-			spec: api.PXCSpec{
-				PodSpec: &api.PodSpec{
-					Image:             "test-image",
-					ImagePullPolicy:   corev1.PullIfNotPresent,
-					EnvVarsSecretName: "test-secret",
-				},
-			},
-			secrets:           "monitor-secret",
-			expectedName:      "pxc",
-			expectedImage:     "test-image",
-			expectedLDPreload: "",
-			expectedEnvFrom: []corev1.EnvFromSource{
-				{
-					SecretRef: &corev1.SecretEnvSource{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: "test-secret",
+		"latest cr container construction ": {
+			spec: api.PerconaXtraDBClusterSpec{
+				CRVersion: version.Version(),
+				PXC: &api.PXCSpec{
+					PodSpec: &api.PodSpec{
+						Image:           "test-image",
+						ImagePullPolicy: corev1.PullIfNotPresent,
+						LivenessProbes: corev1.Probe{
+							TimeoutSeconds: 5,
 						},
-						Optional: ptr.To(true),
+						ReadinessProbes: corev1.Probe{
+							TimeoutSeconds: 15,
+						},
+						EnvVarsSecretName: "test-secret",
 					},
 				},
 			},
-		},
-		"allocator - jemalloc": {
-			spec: api.PXCSpec{
-				MySQLAllocator: "jemalloc",
-				PodSpec: &api.PodSpec{
-					Image:             "test-image",
-					ImagePullPolicy:   corev1.PullIfNotPresent,
-					EnvVarsSecretName: "test-secret",
-				},
+			expectedContainer: func() corev1.Container {
+				return defaultExpectedContainer()
 			},
-			secrets:           "monitor-secret",
-			expectedName:      "pxc",
-			expectedImage:     "test-image",
-			expectedLDPreload: "/usr/lib64/libjemalloc.so.1",
-			expectedEnvFrom: []corev1.EnvFromSource{
-				{
-					SecretRef: &corev1.SecretEnvSource{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: "test-secret",
+		},
+		"container construction with jemalloc": {
+			spec: api.PerconaXtraDBClusterSpec{
+				CRVersion: version.Version(),
+				PXC: &api.PXCSpec{
+					MySQLAllocator: "jemalloc",
+					PodSpec: &api.PodSpec{
+						Image:           "test-image",
+						ImagePullPolicy: corev1.PullIfNotPresent,
+						LivenessProbes: corev1.Probe{
+							TimeoutSeconds: 5,
 						},
-						Optional: ptr.To(true),
+						ReadinessProbes: corev1.Probe{
+							TimeoutSeconds: 15,
+						},
+						EnvVarsSecretName: "test-secret",
 					},
 				},
 			},
-		},
-		"allocator - tcmalloc": {
-			spec: api.PXCSpec{
-				MySQLAllocator: "tcmalloc",
-				PodSpec: &api.PodSpec{
-					Image:             "test-image",
-					ImagePullPolicy:   corev1.PullIfNotPresent,
-					EnvVarsSecretName: "test-secret",
-				},
+			expectedContainer: func() corev1.Container {
+				c := defaultExpectedContainer()
+				c.Env = append(c.Env, corev1.EnvVar{
+					Name:  "LD_PRELOAD",
+					Value: "/usr/lib64/libjemalloc.so.1",
+				})
+				return c
 			},
-			secrets:           "monitor-secret",
-			expectedName:      "pxc",
-			expectedImage:     "test-image",
-			expectedLDPreload: "/usr/lib64/libtcmalloc.so",
-			expectedEnvFrom: []corev1.EnvFromSource{
-				{
-					SecretRef: &corev1.SecretEnvSource{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: "test-secret",
+		},
+		"container construction with tcmalloc": {
+			spec: api.PerconaXtraDBClusterSpec{
+				CRVersion: version.Version(),
+				PXC: &api.PXCSpec{
+					MySQLAllocator: "tcmalloc",
+					PodSpec: &api.PodSpec{
+						Image:           "test-image",
+						ImagePullPolicy: corev1.PullIfNotPresent,
+						LivenessProbes: corev1.Probe{
+							TimeoutSeconds: 5,
 						},
-						Optional: ptr.To(true),
+						ReadinessProbes: corev1.Probe{
+							TimeoutSeconds: 15,
+						},
+						EnvVarsSecretName: "test-secret",
 					},
 				},
 			},
+			expectedContainer: func() corev1.Container {
+				c := defaultExpectedContainer()
+				c.Env = append(c.Env, corev1.EnvVar{
+					Name:  "LD_PRELOAD",
+					Value: "/usr/lib64/libtcmalloc.so",
+				})
+				return c
+			},
 		},
-		"allocator - override with envFrom": {
-			spec: api.PXCSpec{
-				MySQLAllocator: "jemalloc",
-				PodSpec: &api.PodSpec{
-					Image:             "test-image",
-					ImagePullPolicy:   corev1.PullIfNotPresent,
-					EnvVarsSecretName: "test-secret",
+		"container construction with cr configured with jemalloc but priority goes to tcmalloc from envFromSecret": {
+			spec: api.PerconaXtraDBClusterSpec{
+				CRVersion: version.Version(),
+				PXC: &api.PXCSpec{
+					MySQLAllocator: "jemalloc",
+					PodSpec: &api.PodSpec{
+						Image:           "test-image",
+						ImagePullPolicy: corev1.PullIfNotPresent,
+						LivenessProbes: corev1.Probe{
+							TimeoutSeconds: 5,
+						},
+						ReadinessProbes: corev1.Probe{
+							TimeoutSeconds: 15,
+						},
+						EnvVarsSecretName: "test-secret",
+					},
 				},
 			},
-			secrets: "monitor-secret",
 			envFromSecret: &corev1.Secret{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "test-secret",
@@ -115,18 +128,41 @@ func TestPXCAppContainer(t *testing.T) {
 					"LD_PRELOAD": []byte("/usr/lib64/libtcmalloc.so"),
 				},
 			},
-			expectedName:      "pxc",
-			expectedImage:     "test-image",
-			expectedLDPreload: "/usr/lib64/libtcmalloc.so",
-			expectedEnvFrom: []corev1.EnvFromSource{
-				{
-					SecretRef: &corev1.SecretEnvSource{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: "test-secret",
-						},
-						Optional: ptr.To(true),
+			expectedContainer: func() corev1.Container {
+				c := defaultExpectedContainer()
+				c.Env = append(c.Env, corev1.EnvVar{
+					Name:  "LD_PRELOAD",
+					Value: "/usr/lib64/libtcmalloc.so",
+				})
+				return c
+			},
+		},
+		"cr <1.19 with proxysql enabled": {
+			spec: api.PerconaXtraDBClusterSpec{
+				CRVersion: "1.18.0",
+				ProxySQL: &api.ProxySQLSpec{
+					PodSpec: api.PodSpec{
+						Enabled: true,
 					},
 				},
+				PXC: &api.PXCSpec{
+					PodSpec: &api.PodSpec{
+						Image:           "test-image",
+						ImagePullPolicy: corev1.PullIfNotPresent,
+						LivenessProbes: corev1.Probe{
+							TimeoutSeconds: 5,
+						},
+						ReadinessProbes: corev1.Probe{
+							TimeoutSeconds: 15,
+						},
+						EnvVarsSecretName: "test-secret",
+					},
+				},
+			},
+			expectedContainer: func() corev1.Container {
+				c := defaultExpectedContainer()
+				c.Env[9].Value = "mysql_native_password"
+				return c
 			},
 		},
 	}
@@ -137,12 +173,12 @@ func TestPXCAppContainer(t *testing.T) {
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "test-cluster",
 					Namespace: "test-ns",
+					UID:       "test-uid",
 				},
-				Spec: api.PerconaXtraDBClusterSpec{
-					CRVersion: version.Version(),
-					PXC:       &tt.spec,
-				},
+				Spec: tt.spec,
 			}
+
+			pxcNode := Node{cr: cr}
 
 			objs := []runtime.Object{cr}
 			if tt.envFromSecret != nil {
@@ -151,22 +187,76 @@ func TestPXCAppContainer(t *testing.T) {
 
 			client := test.BuildFakeClient(objs...)
 
-			pxcNode := Node{cr: cr}
-
-			c, err := pxcNode.AppContainer(t.Context(), client, tt.spec.PodSpec, tt.secrets, cr, nil)
-			require.NoError(t, err)
-
-			assert.Equal(t, tt.expectedName, c.Name)
-			assert.Equal(t, tt.expectedImage, c.Image)
-			assert.Equal(t, tt.expectedEnvFrom, c.EnvFrom)
-
-			ldPreload := ""
-			for _, e := range c.Env {
-				if e.Name == "LD_PRELOAD" {
-					ldPreload = e.Value
-				}
-			}
-			assert.Equal(t, tt.expectedLDPreload, ldPreload)
+			c, err := pxcNode.AppContainer(t.Context(), client, tt.spec.PXC.PodSpec, secretName, cr, nil)
+			assert.Equal(t, tt.expectedContainer(), c)
+			assert.NoError(t, err)
 		})
+	}
+}
+
+func defaultExpectedContainer() corev1.Container {
+	return corev1.Container{
+		Name:            "pxc",
+		Image:           "test-image",
+		ImagePullPolicy: corev1.PullIfNotPresent,
+		Command:         []string{"/var/lib/mysql/pxc-entrypoint.sh"},
+		Args:            []string{"mysqld"},
+		Ports: []corev1.ContainerPort{
+			{ContainerPort: 3306, Name: "mysql"},
+			{ContainerPort: 4444, Name: "sst"},
+			{ContainerPort: 4567, Name: "write-set"},
+			{ContainerPort: 4568, Name: "ist"},
+			{ContainerPort: 33062, Name: "mysql-admin"},
+			{ContainerPort: 33060, Name: "mysqlx"},
+		},
+		VolumeMounts: []corev1.VolumeMount{
+			{Name: app.DataVolumeName, MountPath: "/var/lib/mysql"},
+			{Name: "config", MountPath: "/etc/percona-xtradb-cluster.conf.d"},
+			{Name: "tmp", MountPath: "/tmp"},
+			{Name: "ssl", MountPath: "/etc/mysql/ssl"},
+			{Name: "ssl-internal", MountPath: "/etc/mysql/ssl-internal"},
+			{Name: "mysql-users-secret-file", MountPath: "/etc/mysql/mysql-users-secret"},
+			{Name: "auto-config", MountPath: "/etc/my.cnf.d"},
+			{Name: VaultSecretVolumeName, MountPath: "/etc/mysql/vault-keyring-secret"},
+			{Name: "mysql-init-file", MountPath: "/etc/mysql/init-file"},
+		},
+		Env: []corev1.EnvVar{
+			{Name: "PXC_SERVICE", Value: "test-cluster-pxc-unready"},
+			{Name: "MONITOR_HOST", Value: "%"},
+			{Name: "MYSQL_ROOT_PASSWORD", ValueFrom: &corev1.EnvVarSource{
+				SecretKeyRef: app.SecretKeySelector("my-secret", users.Root),
+			}},
+			{Name: "XTRABACKUP_PASSWORD", ValueFrom: &corev1.EnvVarSource{
+				SecretKeyRef: app.SecretKeySelector("my-secret", users.Xtrabackup),
+			}},
+			{Name: "MONITOR_PASSWORD", ValueFrom: &corev1.EnvVarSource{
+				SecretKeyRef: app.SecretKeySelector("my-secret", users.Monitor),
+			}},
+			{Name: "CLUSTER_HASH", Value: "2584669"},
+			{Name: "OPERATOR_ADMIN_PASSWORD", ValueFrom: &corev1.EnvVarSource{
+				SecretKeyRef: app.SecretKeySelector("my-secret", users.Operator),
+			}},
+			{Name: "LIVENESS_CHECK_TIMEOUT", Value: "5"},
+			{Name: "READINESS_CHECK_TIMEOUT", Value: "15"},
+			{Name: "DEFAULT_AUTHENTICATION_PLUGIN", Value: "caching_sha2_password"},
+			{Name: "MYSQL_NOTIFY_SOCKET", Value: "/var/lib/mysql/notify.sock"},
+			{Name: "MYSQL_STATE_FILE", Value: "/var/lib/mysql/mysql.state"},
+		},
+		EnvFrom: []corev1.EnvFromSource{
+			{
+				SecretRef: &corev1.SecretEnvSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: "test-secret",
+					},
+					Optional: pointerToTrue(),
+				},
+			},
+		},
+		ReadinessProbe: app.Probe(&corev1.Probe{
+			TimeoutSeconds: 15,
+		}, "/var/lib/mysql/readiness-check.sh"),
+		LivenessProbe: app.Probe(&corev1.Probe{
+			TimeoutSeconds: 5,
+		}, "/var/lib/mysql/liveness-check.sh"),
 	}
 }

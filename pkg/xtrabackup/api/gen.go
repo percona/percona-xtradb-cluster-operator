dae2331e117d2bc43694a4a3b44426a7abe74d28
Mayank Shah <mayank.shah@percona.com>
2025-12-02 16:47:39 +0530

    fix gen.go
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- pkg/xtrabackup/api/gen.go
+++ pkg/xtrabackup/api/gen.go
@@ -1,3 +1,4 @@
 package api
 
-//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative app.proto
+// Run `make protoc` to install protoc and protoc-gen-go
+//go:generate ../../../bin/protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative app.proto

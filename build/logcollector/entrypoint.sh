8fbf63f886bf9e1a296fffe21fa285d05de84132
Mayank Shah <mayank.shah@percona.com>
2026-03-16 16:50:03 +0530

    cleanup
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- build/logcollector/entrypoint.sh
+++ build/logcollector/entrypoint.sh
@@ -102,20 +102,3 @@ case "$1" in
 		exit 1
 		;;
 esac
-
-# if [  "$1" = 'logrotate' ]; then
-#     if [[ $EUID != 1001 ]]; then
-#         # logrotate requires UID in /etc/passwd
-#         sed -e "s^x:1001:^x:$EUID:^" /etc/passwd > /tmp/passwd
-#         cat /tmp/passwd > /etc/passwd
-#         rm -rf /tmp/passwd
-#     fi
-#     exec go-cron "0 0 * * *" sh -c "logrotate -s /opt/percona/logrotate/logrotate.status /opt/percona/logrotate/logrotate-$SERVICE_TYPE.conf;/usr/bin/find /var/lib/mysql/ -name GRA_*.log -mtime +7 -delete"
-# else
-#     if [ "$1" = 'fluent-bit' ]; then
-#         fluentbit_opt+='-c /opt/percona/logcollector/fluentbit/fluentbit.conf'
-#     fi
-
-#     test -e /opt/percona/hookscript/hook.sh && source /opt/percona/hookscript/hook.sh
-#     exec "$@" $fluentbit_opt
-# fi

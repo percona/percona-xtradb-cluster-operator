07c06616fe8838bebbcdb98bc3bd363fbb1d0eca
Mayank Shah <mayank.shah@percona.com>
2026-03-18 09:55:09 +0530

    Update build/logcollector/logrotate/postrotate-mysql.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- build/logcollector/logrotate/postrotate-mysql.sh
+++ build/logcollector/logrotate/postrotate-mysql.sh
@@ -14,7 +14,7 @@ export MYSQL_PWD=${MONITOR_PASSWORD}
 # Check if the audit plugin is loaded
 audit_plugin_loaded=$($MYSQL_CMDLINE -e "SHOW PLUGINS" | grep -c 'audit_log' || true)
 if [ $audit_plugin_loaded -gt 0 ]; then
-    $MYSQL_CMDLINE -e 'FLUSH NO_WRITE_TO_BINLOG ERROR LOGS;SET GLOBAL audit_log_flush=1;'
+	$MYSQL_CMDLINE -e 'FLUSH NO_WRITE_TO_BINLOG ERROR LOGS;SET GLOBAL audit_log_flush=1;'
 else
 	$MYSQL_CMDLINE -e 'FLUSH NO_WRITE_TO_BINLOG ERROR LOGS;'
 fi

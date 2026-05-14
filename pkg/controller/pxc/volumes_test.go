1d7c4194edf336daad30ca3677e20b8df97736ff
Andrii Dema <a.dema@jazzserve.com>
2026-05-14 13:31:21 +0300

    fix unit-test

--- pkg/controller/pxc/volumes_test.go
+++ pkg/controller/pxc/volumes_test.go
@@ -329,8 +329,8 @@ func TestReconcilePersistentVolumesVolumeExternalAutoscaling(t *testing.T) {
 
 			cr.Spec.StorageScaling = &pxcv1.StorageScalingSpec{
 				VolumeExternalAutoscaling: tt.volumeExternalAutoscaling,
+				EnableVolumeScaling:       tt.volumeExpansionEnabled,
 			}
-			cr.Spec.VolumeExpansionEnabled = tt.volumeExpansionEnabled
 
 			cl := fake.NewClientBuilder().
 				WithScheme(scheme).

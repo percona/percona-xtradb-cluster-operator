1cd334bed3cc58e68816c9339820f916fc1dc774
Andrii Dema <a.dema@jazzserve.com>
2026-05-22 12:08:37 +0300

    fix test

--- pkg/controller/pxc/volumes.go
+++ pkg/controller/pxc/volumes.go
@@ -257,7 +257,7 @@ func (r *ReconcilePerconaXtraDBCluster) reconcilePersistentVolumes(ctx context.C
 		if err := r.revertVolumeTemplate(ctx, cr, configured); err != nil {
 			return errors.Wrapf(err, "revert volume template in pxc/%s", cr.Name)
 		}
-		if cr.Spec.VolumeExpansionEnabled {
+		if cr.Spec.IsVolumeExpansionEnabled() {
 			return errors.Errorf("requested storage (%s) is less than actual storage (%s)", requested.String(), actual.String())
 		}
 	}
